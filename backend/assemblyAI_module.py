f18dded0e6af43e21cbb6d5905764c3618568b9c
Parul Garg <pgar0011@student.monash.edu>
2024-04-21 18:50:25 +1000

    Added function to transcribe audio in assemblyAI_module.py

--- backend/assemblyAI_module.py
+++ backend/assemblyAI_module.py
@@ -1,1 +1,24 @@
 """ This is the module that stores all functions related to Assembly AI"""
+
+import assemblyai as aai
+
+aai.settings.api_key = "620d4459badf4480a039c646ab4c3216"
+
+def  transcribe(audioURL) -> str:
+    '''This function takes an audio file and returns its transcript.
+    
+    Parameters:
+        audioURL (str): The URL of the audio file you want to transcribe.
+        
+    Returns:
+        str: Transcription text from the audio file.
+    
+    '''
+    transcriber = aai.Transcriber()
+    transcript = transcriber.transcribe(audioURL)
+
+    if transcript.status == aai.TranscriptStatus.error:
+        return transcript.error
+    else:
+        return transcript.text 
+
