2a9f090d96de5a71ba41bebaceccd6f803ccda16
ZWAN0318 <zwan0318@student.monash.edu>
2024-04-22 13:42:42 +1000

    Transcription with audiofile function
    Implemented function to transcribe audiofile with speakers

--- backend/assemblyAI_module.py
+++ backend/assemblyAI_module.py
@@ -4,7 +4,7 @@ import assemblyai as aai
 
 aai.settings.api_key = "620d4459badf4480a039c646ab4c3216"
 
-def  transcribe(audioURL) -> str:
+def transcribe(audioURL) -> str:
     '''This function takes an audio file and returns its transcript.
     
     Parameters:
@@ -21,4 +21,28 @@ def  transcribe(audioURL) -> str:
         return transcript.error
     else:
         return transcript.text 
+
+def transcribe_with_speakers(audioUrl) -> str:
+    '''This function takes an audio file and returns its transcript with speakers.
+    
+    Parameters:
+        audioURL (str): The URL of the audio file you want to transcribe.
+        
+    Returns:
+        str: Transcription text with speakers from the audio file.
+    
+    '''
+    transcriber = aai.Transcriber()
+    config = aai.TranscriptionConfig(speaker_labels=True)
+    transcript = transcriber.transcribe(audioUrl, config)
+
+    if transcript.status == aai.TranscriptStatus.error:
+        return transcript.error
+
+    transcript_with_speakers = ""
+
+    for utterance in transcript.utterances:
+        transcript_with_speakers += f"Speaker {utterance.speaker}: {utterance.text}\n"
+
+    return transcript_with_speakers
 
