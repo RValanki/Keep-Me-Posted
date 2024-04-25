6fd0ad279e42b3a88c3b32c284ed36c1ca30eca0
Rohit Valanki <rval00008@student.monash.edu>
2024-04-25 15:49:36 +1000

    Updated login endpoint to authenticate with email instead of username

--- backend/kmp_backend/Authentication/views.py
+++ backend/kmp_backend/Authentication/views.py
@@ -11,7 +11,7 @@ from rest_framework.permissions import IsAuthenticated
 
 @api_view(['POST'])
 def login(request):
-    user = get_object_or_404(User, username=request.data['username'])
+    user = get_object_or_404(User, email=request.data['email'])
     if not user.check_password(request.data['password']):
         return Response({"detail": "Not found!"}, status=status.HTTP_404_NOT_FOUND)
 
