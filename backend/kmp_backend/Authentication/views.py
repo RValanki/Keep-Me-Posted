392a0bd5a20aff94a4b10077123180cafbbd1ffc
bche0062 <bche0062@student.monash.edu>
2024-04-28 14:49:05 +1000

    added docstring to views.py

--- backend/kmp_backend/Authentication/views.py
+++ backend/kmp_backend/Authentication/views.py
@@ -11,6 +11,19 @@ from rest_framework.permissions import IsAuthenticated
 
 @api_view(['POST'])
 def login(request):
+    """
+    Endpoint for user login.
+
+    This view receives a POST request with user credentials (email and password).
+    It checks if the provided credentials are valid and returns a token if authentication is successful.
+
+    Args:
+        request (HttpRequest): The incoming HTTP request object containing user credentials.
+
+    Returns:
+        Response: JSON response with authentication token and user details if authentication is successful,
+                  or an error message with HTTP status code if authentication fails.
+    """
     user = get_object_or_404(User, email=request.data['email'])
     if not user.check_password(request.data['password']):
         return Response({"detail": "Not found!"}, status=status.HTTP_404_NOT_FOUND)
@@ -21,7 +34,20 @@ def login(request):
 
 @api_view(['POST'])
 def signup(request):
-    #If email is use bad request
+    """
+    Endpoint for user registration.
+
+    This view receives a POST request with user details (username, email, and password).
+    It checks if the provided email is already in use, and if not, registers the user and returns a token.
+
+    Args:
+        request (HttpRequest): The incoming HTTP request object containing user details.
+
+    Returns:
+        Response: JSON response with authentication token and user details if registration is successful,
+                  or an error message with HTTP status code if registration fails.
+    """
+    # Check if email is already in use
     if User.objects.filter(email=request.data['email']).exists():
         return Response({"error": "This email address is already in use."}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -35,11 +61,23 @@ def signup(request):
         token = Token.objects.create(user=user)
         return Response({"token": token.key, "user": serializer.data})
 
-    #Not valid
+    # Return error if serializer data is not valid
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication, TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def test_token(request):
+    """
+    Test endpoint for token authentication.
+
+    This view is protected and requires a valid authentication token to access.
+    It returns a simple message confirming successful token-based authentication.
+
+    Args:
+        request (HttpRequest): The incoming HTTP request object containing authentication token.
+
+    Returns:
+        Response: JSON response with a success message indicating successful token authentication.
+    """
     return Response("passed for {}".format(request.user.email))
