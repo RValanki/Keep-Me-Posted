cb20900ea0b616369165d156e2c7b18b69ada93f
Harrison Lane <hlan0016@student.monash.edu>
2024-05-02 11:34:13 +1000

    styled email page

--- frontend/src/routes/email/+page.svelte
+++ frontend/src/routes/email/+page.svelte
@@ -1,120 +1,163 @@
 <script>
+  const postRequestString = "http://localhost:5173/"; // yours may be different, see what link pops up when you run django server, then add /emailer on the end
 
-    let postRequestString = "http://localhost:5173/"  // yours may be different, see what link pops up when you run django server, then add /emailer on the end
-    let contacts = []
-    let contents = ""
-    let emailField = ""
-    let errorMessage = ""
-    let name = ""
+  let contacts = [];
 
-    
-    let buttonPressed = () => {
-        let data = new FormData()
-        data.append('contents', contents)
-        data.append('contacts', contacts)
-            
-        fetch(postRequestString, {method: 'POST', data: data}).then(response => {
-            console.log(response)
-        })
+  let messageField = "";
+  let emailField = ""
+
+  let errorMessage = "";
+
+  let buttonPressed = () => {
+    if (!(contacts.length) || !messageField) {
+      errorMessage = "Please enter a message and add contacts.";
+      // Clear error message after 2 seconds
+      setTimeout(() => {
+        errorMessage = "";
+      }, 2000);
+
+      return
     }
 
+    let data = new FormData();
+    contacts = contacts
+    messageField = messageField
 
-    /**
-     * This function is used to remove the email from the array and display when the user clicks the delete
-     * @param index
-     */
-    let remove_email= (index)=> {
-        contacts.splice(index, 1)
-        contacts = contacts
+    data.append("message", messageField);
+    data.append("contacts", contacts);
+
+    // fetch(postRequestString, { method: "POST", data: data }).then(
+    //   (response) => {
+    //     console.log(response);
+    //   }
+    // );
+  };
+
+  /**
+   * This function is used to remove the email from the array and display when the user clicks the delete
+   * @param index
+   */
+  let remove_email = (index) => {
+    contacts.splice(index, 1);
+    contacts = contacts;
+  };
+
+  /**
+   * add_email function is for displaying the user emails in the display field and for adding the user to the contact array
+   */
+  let add_email = () => {
+    if (!emailField.trim()) {
+      //This is to display error message when inputfield is empty
+      errorMessage = "Please enter email address.";
+      // Clear error message after 2 seconds
+      setTimeout(() => {
+        errorMessage = "";
+      }, 2000);
+    } else {
+      //if input field is not empty then it will be pushed to the contacts list
+      contacts.push(emailField);
+      emailField = "";
+      contacts = contacts;
     }
-
-    /**
-     * add_email function is for displaying the user emails in the display field and for adding the user to the contact array
-     */
-    let add_email = () =>{
-        if(!emailField.trim()){
-            //This is to display error message when inputfield is empty
-            errorMessage = 'Please enter email address.';
-            // Clear error message after 2 seconds
-            setTimeout(() => {
-                errorMessage = '';
-            }, 2000);
-        }
-        else{
-            //if input field is not empty then it will be pushed to the contacts list
-            contacts.push(emailField)
-            emailField = ""
-            contacts = contacts
-            name = name
-        }
-        
-    }
-
-    
+  };
 </script>
 
-<input bind:value = {name} placeholder = "enter your email address" />
-<p>Hello {name || 'welcome to KeepMePosted'}!</p>
-
-
-
 <!-- This link is currently used for using google material icons-->
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      rel="stylesheet">
+<link
+  href="https://fonts.googleapis.com/icon?family=Material+Icons"
+  rel="stylesheet"
+/>
 
-      <!-- this styling secton contains styles for many components used throughout the page-->
+<body class="container">
+  <div class="message">
+    <p>Hello welcome to KeepMePosted!</p>
+    <textarea placeholder="Enter your message here" bind:value={messageField}></textarea>
+  </div>
+  <!--If an error message is triggered the error message is displayed-->
+
+  <div class="emails">
+    <div class="emails-add">
+      <label for="email">Enter Contact's Email Address:</label>
+      <input placeholder="test@example.com" bind:value={emailField} />
+      <button on:click={add_email}>Add +</button>
+    </div>
+
+    <!-- loops through each contact and prints -->
+    <div class="emails-list">
+      {#each contacts as email, index (index)}
+        <div class="emails-item">
+          <p>{email}</p>
+          <button class="remove-button" on:click={remove_email(index)}>-</button
+          >
+        </div>
+      {/each}
+    </div>
+  </div>
+
+  <div class="send">
+    <button type="button" on:click={buttonPressed}> Send </button>
+  </div>
+
+  <div class="error">
+    {#if errorMessage}
+      <p class="error">{errorMessage}</p>
+    {/if}
+  </div>
+</body>
+
+<!-- this styling secton contains styles for many components used throughout the page-->
 <style>
-    .heading{
-        text-align: center;
-    }
+  body {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 50px;
+  }
 
-    ul {
-        list-style-type: none;
-    }   
+  textarea {
+    width: 100%;
+    height: 100px;
+  }
 
-    .email {
-        display: flex;
-        justify-content: center;
-    }
-    
-    .error {
-        color: red;
-        position: fixed;
-        bottom: 30px;
-        left: 50%;
-        transform: translateX(-50%);
-    }
+  .emails {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 10px;
+  }
+  .emails-add {
+    display: flex;
+    gap: 10px;
+  }
 
+  .emails-list {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .emails-item {
+    display: flex;
+    height: 30px;
+    gap: 30px;
+    align-items: center;
+  }
+
+  .remove-button {
+    border-radius: 50px;
+    border: 3px lightGrey;
+    cursor: pointer;
+    background-color: lightblue;
+    height: 20px;
+  }
+
+  .error {
+    width: 100%;
+    text-align: center;
+    color: red;
+    position: fixed;
+    bottom: 30px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
 </style>
-
-<br>
-<!--If an error message is triggered the error message is displayed-->
-{#if errorMessage}
-<p class="error">{errorMessage}</p>
-{/if}
-
-
-<div class="heading">Add Receipents<br><br><br><br><br><br>
-<!-- Below is HTML code for the input field and add button-->
-<label for = "email">Enter User Email Address:</label>
-<input placeholder = "test@example.com" bind:value={emailField}>
-
-
-<!-- on click the add button calls the add_email function-->
-<button on:click={add_email}>Add +</button><br><br><br><br><br>
-
-<!-- loops through each contact and prints -->
-<ul>
-    {#each contacts as email, index (index)}
-    <div class="email" style="margin-top:10px;"><li>{email}</li><button style="border-radius:50px; border:3px lightGrey; cursor:pointer;background-color:lightblue;" on:click={remove_email(index)}>-</button></div>
-    {/each}
-</ul>
-
-<button type="button" on:click={buttonPressed}> Send </button>
-
-<div id="display" >
-
-
-
-</div>
-</div>
