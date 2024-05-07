5a74448e1143026f8107eb1157f7eb74fe129955
bowendong1234 <bdon0009@student.monash.edu>
2024-05-07 12:50:46 +1000

    Added skeleton code for card component + basic testing

---
+++ frontend/src/components/card.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let cardText;
+    export let cardHeadingText;
+    export let cardOutlineColor = "black";
+    export let cardBackgroundColor = "white";
+    export let onCardClick;
+    export let cardDestination = "/";
+    
+    const handleClick = () => {
+        if(onCardClick) onClick();
+            // Navigate to another page
+            window.location.href = cardDestination;
+    };
+</script>
+  
+<button
+    class="Card"
+    style="
+        background-color: {cardBackgroundColor};
+        border: 2px solid {cardOutlineColor};
+        border-radius: 10px;
+    "
+    on:click={handleClick}
+    >
+    <h2>{cardHeadingText}</h2>
+    <p>{cardText}</p>
+</button>
+  
+<style>
+    .Card {
+        padding: 10px 20px;
+        cursor: pointer;
+        transition: background-color 0.3s, color 0.3s, border-color 0.3s;
+    }
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,18 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Card from '../components/card.svelte';
+    
+    const navigateTo = () => {
+      // go to send asap page or the other one i forgot when user clicks
+    };
+  </script>
+  
+<Card
+    cardText="We will send the summary to your recipients as soon as it is generated. You can just sit back and relax"
+    cardHeadingText="Send Summary Asap"
+    cardOutlineColor="orange"
+    cardBackgroundColor="lightyellow"
+    onCardClick={navigateTo}
+    cardDestination="/another-page"
+/>
