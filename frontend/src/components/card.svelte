d7cf7248fb7fffbb499ff2164e12b4047781ec5d
bowendong1234 <bdon0009@student.monash.edu>
2024-05-09 15:16:13 +1000

    Made the card component scale respective to the size of the screen

--- frontend/src/components/card.svelte
+++ frontend/src/components/card.svelte
@@ -5,8 +5,8 @@
     export let cardBackgroundColor;
     export let cardDestination;
     export let cardTextColour;
-    export let cardWidth = "300px"; //default dimensions
-    export let cardHeight = "300px";
+    export let cardWidth = "30%"; //default dimensions
+    export let cardHeight = "60%";
     export let cardImagePath;
     
     const handleClick = () => {
@@ -38,27 +38,49 @@
     }
 </style>
 
-<!-- Use this to initialse the cards -->
+<!-- The code below can be used to put the cards on the screen, in order for the cards to be size responsive they need
+to be in some kind of layout/container/wrapper. If you put the code as it is on a page the cards will initialise
+on top of each other -->
+
 <!-- <script>
     import Card from '../components/card.svelte';
 </script>
-This is for the send asap card
-<Card
-    cardText="We will send the summary to your recipients as soon as it is generated. You can just sit back and relax"
-    cardHeadingText="Send Summary Asap"
-    cardOutlineColor="#FDDCAB"
-    cardBackgroundColor="#FFFAF5"
-    cardDestination="/"
-    cardTextColour= "#EC4A0A"
-    cardImagePath= '/send_asap_lightning.png'
-/>
-THis is for the customise card
-<Card
-    cardText="You can customise the email sent to your recipients and the summary will not be sent until you choose."
-    cardHeadingText="Customise"
-    cardOutlineColor="#FCCEEE"
-    cardBackgroundColor="#FAFAFF"
-    cardDestination="/"
-    cardTextColour= "#C11574"
-    cardImagePath= '/customise_pen.png'
-/> -->
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        height: 100vh; /* Make ontainer fill the viewport height which is the visible screen pretty much */
+    }
+    .button-wrapper {
+        flex-grow: 1; /* 1 makes the wrapper grow to fill the remaining space */
+    }
+</style>
+
+<div class="container">
+    <div class="button-wrapper">
+        <Card
+            cardText="We will send the summary to your recipients as soon as it is generated. You can just sit back and relax"
+            cardHeadingText="Send Summary Asap"
+            cardOutlineColor="#FDDCAB"
+            cardBackgroundColor="#FFFAF5"
+            cardDestination="/"
+            cardTextColour= "#EC4A0A"
+            cardImagePath= '/send_asap_lightning.png'
+        />
+    </div>
+</div>
+
+<div class="container">
+    <div class="button-wrapper">
+        <Card
+            cardText="You can customise the email sent to your recipients and the summary will not be sent until you choose."
+            cardHeadingText="Customise"
+            cardOutlineColor="#FCCEEE"
+            cardBackgroundColor="#FAFAFF"
+            cardDestination="/"
+            cardTextColour= "#C11574"
+            cardImagePath= '/customise_pen.png'
+        />  
+    </div>
+</div> -->
