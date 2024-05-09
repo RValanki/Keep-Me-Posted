5be9fb93b3e3b751ca1c38c2b5597e11b6203aa0
bowendong1234 <bdon0009@student.monash.edu>
2024-05-09 14:31:28 +1000

    Styled card component, cleaned up navigation method, added static assets

--- frontend/src/components/card.svelte
+++ frontend/src/components/card.svelte
@@ -1,15 +1,16 @@
 <script>
     export let cardText;
     export let cardHeadingText;
-    export let cardOutlineColor = "black";
-    export let cardBackgroundColor = "white";
-    export let onCardClick;
-    export let cardDestination = "/";
+    export let cardOutlineColor;
+    export let cardBackgroundColor;
+    export let cardDestination;
+    export let cardTextColour;
+    export let cardWidth = "300px"; //default dimensions
+    export let cardHeight = "300px";
+    export let cardImagePath;
     
     const handleClick = () => {
-        if(onCardClick) onClick();
-            // Navigate to another page
-            window.location.href = cardDestination;
+        // window.location.href = cardDestination;
     };
 </script>
   
@@ -19,11 +20,14 @@
         background-color: {cardBackgroundColor};
         border: 2px solid {cardOutlineColor};
         border-radius: 10px;
+        width: {cardWidth};
+        height: {cardHeight};
     "
     on:click={handleClick}
     >
-    <h2>{cardHeadingText}</h2>
-    <p>{cardText}</p>
+    <img src={cardImagePath} alt="card icon" style="max-width: 100%; max-height: 100%;" />
+    <h2 style="color: {cardTextColour};">{cardHeadingText}</h2>
+    <p style="color: {cardTextColour};">{cardText}</p>
 </button>
   
 <style>
@@ -33,3 +37,28 @@
         transition: background-color 0.3s, color 0.3s, border-color 0.3s;
     }
 </style>
+
+<!-- Use this to initialse the cards -->
+<!-- <script>
+    import Card from '../components/card.svelte';
+</script>
+This is for the send asap card
+<Card
+    cardText="We will send the summary to your recipients as soon as it is generated. You can just sit back and relax"
+    cardHeadingText="Send Summary Asap"
+    cardOutlineColor="#FDDCAB"
+    cardBackgroundColor="#FFFAF5"
+    cardDestination="/"
+    cardTextColour= "#EC4A0A"
+    cardImagePath= '/send_asap_lightning.png'
+/>
+THis is for the customise card
+<Card
+    cardText="You can customise the email sent to your recipients and the summary will not be sent until you choose."
+    cardHeadingText="Customise"
+    cardOutlineColor="#FCCEEE"
+    cardBackgroundColor="#FAFAFF"
+    cardDestination="/"
+    cardTextColour= "#C11574"
+    cardImagePath= '/customise_pen.png'
+/> -->
