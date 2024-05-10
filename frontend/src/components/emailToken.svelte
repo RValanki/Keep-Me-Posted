15e95a2eab05c968f03fff2722ad505a89363947
Ayesha Tariq <atar0014@student.monash.edu>
2024-05-10 23:04:11 +1000

    modifying button code to better suit the delete button + creating email token in progress

---
+++ frontend/src/components/emailToken.svelte
@@ -0,0 +1,38 @@
+<!--Email Token Component -->
+
+<script>
+    import Button from './button.svelte';
+
+    function deleteButtonClick(){
+        
+    }
+
+</script>
+
+<style>
+
+    @import url("https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0");
+
+    .token-body{
+        display: inline-flex;
+        align-items: center;
+        border-radius: 30px;
+        background-color: antiquewhite;
+        padding: 5px 10px;
+        color:black;
+        margin-right: 10px;
+    }
+
+    .token-body-span span {
+        margin-right:5px;
+    }
+</style>
+
+<Button>
+    
+    handleClick = {deleteButtonClick}
+    fullWidth={true}
+
+    <span class = "material-symbols-outlined">close</span>
+
+</Button>
