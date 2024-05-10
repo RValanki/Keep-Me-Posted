885895cd0c8d564bee58077a41c74e75242dccc8
Benjamin Cherian <bche0062@student.monash.edu>
2024-05-10 19:11:25 +1000

    changing upload box logic and added file status

--- frontend/src/components/uploadbox.svelte
+++ frontend/src/components/uploadbox.svelte
@@ -4,7 +4,7 @@
 
     Author: zwan0318
     Edited by: bche0062
-    Last modified: bche0062
+    Last modified: 10/05/2024
 
  -->
 
@@ -20,33 +20,43 @@
 
   function handleFilesSelect(e) {
     const { acceptedFiles } = e.detail;
+    file = null; // Reset the file each time new files are selected
+
     if (acceptedFiles.length > 0) {
       const selectedFile = acceptedFiles[0];
-      if (selectedFile.name.endsWith('.mp3') || selectedFile.name.endsWith('.wav')) {
-        const audio = new Audio(URL.createObjectURL(selectedFile));
-        audio.addEventListener('loadedmetadata', () => {
-          if (audio.duration <= MAX_DURATION_SECONDS) { 
-            file = selectedFile;
-            errorMessage = '';
-          } else {
-            errorMessage = 'File is too long. Please uploa  d a file shorter than 120 minutes.';
-            file = null;
-            //TODO: Replace with error message pop-up
-          }
-        });
-      } else {
-        errorMessage = 'Invalid file type. Please upload a .mp3 or .wav file.';
-        file = null;
-        //TODO: Replace with error message pop-up
+      // Initial file type check before loading it as an audio source
+      if (!selectedFile.name.endsWith('.mp3') && !selectedFile.name.endsWith('.wav')) {
+        errorMessage = 'Invalid audio format! \n Your meeting audio must be in MP3 or WAV format.';
+        return; // Exit the function early if file type is incorrect
       }
+
+      const audio = new Audio(URL.createObjectURL(selectedFile));
+      audio.addEventListener('loadedmetadata', () => {
+        if (audio.duration <= MAX_DURATION_SECONDS) {
+          file = selectedFile;
+          errorMessage = '';
+        } else {
+          errorMessage = 'Meeting duration exceeded! \n Your meeting audio should be less than 120 minutes.';
+          file = null;
+          //TODO: Replace with error message pop-up
+        }
+      });
+    } else {
+      errorMessage = 'Invalid audio format! Your meeting audio must be in MP3 or WAV format';
     }
   }
 </script>
 
-
-<Dropzone on:drop={handleFilesSelect} accept=".mp3, .wav" let:open>
+<Dropzone on:drop={handleFilesSelect} accept=".mp3, .wav">
   <div class="dropzone-label">
     <p>Upload meeting audio</p>
     <p>Must be under 120 minutes. MP3 or WAV formats accepted.</p>
   </div>
 </Dropzone>
+
+{#if file}
+  <p>File ready: {file.name} - {Math.floor(file.size / 1024)} KB</p>
+{:else}
+  <p>{errorMessage}</p>
+{/if}
+
