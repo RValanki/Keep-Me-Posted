5d58865fe239c2690a84d4fcb5b76eed5514b00b
Rohit Valanki <rval00008@student.monash.edu>
2024-05-10 22:14:08 +1000

    Updated signup ui to be responsive to window size

--- frontend/src/routes/signup/+page.svelte
+++ frontend/src/routes/signup/+page.svelte
@@ -157,33 +157,33 @@
  left: 10vw;
  top: 35vh;">
  
-   <div style = "" >
-     <img src= {logo} alt = "Logo">
-   </div>
+   <div style="max-width: 41%;">
+  <img src="{logo}" alt="Logo" style="width: 100%; height: auto;">
+</div>
  
  
  
  
-   <div style = "font-size: 50px;
- font-family: 'Inter', sans-serif;
- font-weight: 550;
- letter-spacing: 0%;
- position: relative; top: 0.2vh;">
-     Keep Me Posted
-   </div>
- 
- 
- 
-   
-   <div style = "font-size: 22px;
- font-family: 'Inter', sans-serif;
- font-weight: 100;
- font-style: italic;
- letter-spacing: 0%;
- position: relative; top: 0.5vh;
- ">
-     Meetings to summaries.
-   </div>
+<div style = "font-size: calc(2vw + 2.5vh);
+font-family: 'Inter', sans-serif;
+font-weight: 550;
+letter-spacing: 0%;
+position: relative; top: 0.2vh;">
+    Keep Me Posted
+  </div>
+
+
+
+  
+  <div style = "font-size: calc(1vw + 0.9vh);
+font-family: 'Inter', sans-serif;
+font-weight: 100;
+font-style: italic;
+letter-spacing: 0%;
+position: relative; top: 0.5vh;
+">
+    Meetings to summaries.
+  </div>
  </div>
  
   
@@ -194,7 +194,7 @@
  
  
  
-   <div style = "height: 75vh; width: 60vh; border-radius: 16px; position: absolute; right: 12vw; background-color: white">
+   <div style = "height: 75vh; width: 32vw; border-radius: 16px; position: absolute; right: 12vw; background-color: white">
     
  
  
@@ -202,13 +202,13 @@
  
      <div style = 
      "width: 60vh; height: 10vh; color: #101828; position: absolute; top: 6vh;
-     font-size: 30px;
+     font-size: calc(1.5vw + 1vh);
   font-family: 'Inter', sans-serif;
   font-weight: 575;
   line-height: 38px;
   letter-spacing: 0%;
   vertical-align: baseline;
-  left: 11vh;
+  left: 5.8vw;
      "
      >
           Sign Up
@@ -219,13 +219,13 @@
      <div
      style = 
      "width: 60vh; height: 10vh; color: #667085; position: absolute; top: 11vh;
-     font-size: 16px;
+     font-size: calc(0.5vw + 1vh);  
      font-family: 'Inter', sans-serif;
      font-weight: 350;
      line-height: 24px;
      letter-spacing: 0%;
   vertical-align: baseline;
-  left: 11vh;
+  left: 5.8vw;
      "
      >
       Create an account!
@@ -237,14 +237,14 @@
      "width: 60vh; height: 10vh; position: absolute; top: 18vh;">
   
           <div style = 
-          "font-size: 14px;
+          "font-size: calc(0.4vw + 1vh);
           font-family: 'Inter', sans-serif;
           font-weight: 500;
           line-height: 20px;
           letter-spacing: 0%;
           vertical-align: baseline;
           position: absolute;
-          left: 11vh;
+          left: 5.8vw;
           color: #344054;
           "
           
@@ -261,23 +261,24 @@
           vertical-align: baseline;
           position: absolute;
           top: 3vh;
-          left: 11vh;
+          left: 5.8vw;
           ">
   
-              <input type="text" bind:value={email} style="border: 1px solid #D0D5DD; border-radius: 5px; padding: 10px; width: 20vw;" placeholder="name@email.com" />
+              <input type="text" bind:value={email} style="font-size: calc(0.4vw + 1vh); border: 1px solid #D0D5DD; border-radius: 5px; padding: 10px; width: 20vw; height: 2vh;" placeholder="name@email.com" />
           </div>
           
           <div style = 
-          "font-size: 10.5px;
+          "font-size: calc(0.4vw + 0.5vh);
           font-family: 'Inter', sans-serif;
           font-weight: 400;
           line-height: 20px;
           letter-spacing: 0%;
           vertical-align: baseline;
           position: absolute;
-          left: 11vh;
+          left: 5.8vw;
           color: red;
           top: 7.5vh;
+          padding-top: 2.2px;
           ">
            {emailValidationString}
           </div>
@@ -291,14 +292,14 @@
      "width: 60vh; height: 10vh; position: absolute; top: 29vh;">
   
           <div style = 
-          "font-size: 14px;
+          "font-size: calc(0.4vw + 1vh);
           font-family: 'Inter', sans-serif;
           font-weight: 500;
           line-height: 20px;
           letter-spacing: 0%;
           vertical-align: baseline;
           position: absolute;
-          left: 11vh;
+          left: 5.8vw;
           color: #344054;
           "
           
@@ -315,24 +316,25 @@
           vertical-align: baseline;
           position: absolute;
           top: 3vh;
-          left: 11vh;
+          left: 5.8vw; 
           ">
   
-              <input type="password" bind:value={password} style="border: 1px solid #D0D5DD; border-radius: 5px; padding: 10px; width: 20vw; letter-spacing: 2px;" placeholder="••••••••" />
+              <input type="password" bind:value={password} style="font-size: calc(0.4vw + 1vh); border: 1px solid #D0D5DD; border-radius: 5px; padding: 10px; width: 20vw; letter-spacing: 2px;" placeholder="••••••••" />
           </div>
  
           <div style = 
-          "font-size: 10.5px;
+          "font-size: calc(0.4vw + 0.5vh);
           font-family: 'Inter', sans-serif;
           font-weight: 400;
-          line-height: 20px;
+          line-height: 2vh;
           letter-spacing: 0%;
           vertical-align: baseline;
           position: absolute;
-          left: 11vh;
+          left: 5.8vw;
           color: {passwordValidationStringColor};
           top: 7.5vh;
           width: 32vh;
+          padding-top: 7px;
           ">
            {passwordValidationString}
           </div>
@@ -343,14 +345,14 @@
      "width: 60vh; height: 10vh; position: absolute; top: 42.5vh;">
   
           <div style = 
-          "font-size: 14px;
+          "font-size: calc(0.4vw + 1vh);
           font-family: 'Inter', sans-serif;
           font-weight: 500;
           line-height: 20px;
           letter-spacing: 0%;
           vertical-align: baseline;
           position: absolute;
-          left: 11vh;
+          left: 5.8vw;
           color: #344054;
           "
           
@@ -367,23 +369,24 @@
           vertical-align: baseline;
           position: absolute;
           top: 3vh;
-          left: 11vh;
+          left: 5.8vw;
           ">
   
-              <input type="password" bind:value={verifyPassword} style="border: 1px solid #D0D5DD; border-radius: 5px; padding: 10px; width: 20vw; letter-spacing: 2px;" placeholder="••••••••" />
+              <input type="password" bind:value={verifyPassword} style="font-size: calc(0.4vw + 1vh); border: 1px solid #D0D5DD; border-radius: 5px; padding: 10px; width: 20vw; letter-spacing: 2px;" placeholder="••••••••" />
           </div>
  
           <div style = 
-          "font-size: 10.5px;
+          "font-size: calc(0.4vw + 0.5vh);
           font-family: 'Inter', sans-serif;
           font-weight: 400;
           line-height: 20px;
           letter-spacing: 0%;
           vertical-align: baseline;
           position: absolute;
-          left: 11vh;
+          left: 5.8vw;
           color: red;
           top: 7.5vh;
+          padding-top: 2.2px;
           ">
             {verifyPasswordValidationString}
           </div>
@@ -401,31 +404,66 @@
      <div style = "
      width: 60vh; height: 10vh;  position: absolute; top: 54vh; 
      ">
-     <button on:click={handleSignUp} style=
-     "background-color: #1570EF; color: #fff; border: none; border-radius: 8px; padding: 10px 20px; cursor: pointer;
-     transition: background-color 0.3s ease-in-out;
-     width: 21.5vw;
-     position: absolute;
-     left: 11vh;
-     font-weight: 575;
-     " 
-     onmouseover="this.style.backgroundColor='#0c4eae'" 
-     onmouseout="this.style.backgroundColor='#1570EF'"
-     >
-     Sign Up
-     </button>
+     <button on:click={handleSignUp} style="
+        background-color: #1570EF; 
+        color: #fff; 
+        border: none; 
+        border-radius: 8px; 
+        padding: 10px 20px; 
+        cursor: pointer;
+        transition: background-color 0.3s ease-in-out;
+        width: 21.5vw;
+        position: absolute;
+        left: 5.8vw;
+        font-weight: 575;
+        font-size: calc(0.5vw + 0.8vh);
+        height: 5vh;
+        text-align: center;
+        display: flex; /* Use flexbox */
+        justify-content: center; /* Center horizontally */
+        align-items: center; /* Center vertically */
+    " 
+    onmouseover="this.style.backgroundColor='#0c4eae'" 
+    onmouseout="this.style.backgroundColor='#1570EF'"
+    >
+    Sign Up
+    </button>
  
      </div>
  
      
  
      <div style="
-     width: 60vh; height: 10vh; position: absolute; top: 60vh;
- ">   <form method="post" action="?/OAuth2">
-     <button style="border: 1px solid #D0D5DD; background-color: white; color: #344054; border-radius: 8px; padding: 10px 20px; cursor: pointer; transition: background-color 0.3s ease-in-out; width: 21.5vw; position: absolute; left: 11vh;" onmouseover="this.style.backgroundColor='#D0D5DD'" onmouseout="this.style.backgroundColor='white'">
-         <img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_92x30px.svg" alt="Google Logo" style="width: 30px; height: auto; margin-right: 10px;">Sign Up With Google
-     </button>
-    </form>
+     width: 60vh; height: 10vh; position: absolute; top: 60.5vh;
+ ">   
+ <form method="post" action="?/OAuth2">
+  <button alt='google sign in' style="
+      height: 5vh; 
+      font-size: calc(0.5vw + 0.8vh); 
+      border: 1px solid #D0D5DD; 
+      background-color: white; 
+      color: #344054; 
+      border-radius: 8px; 
+      padding: 10px 20px; 
+      cursor: pointer; 
+      transition: background-color 0.3s ease-in-out; 
+      width: 21.5vw; 
+      position: absolute; 
+      left: 5.8vw; 
+      display: flex; /* Use flexbox */
+      justify-content: center; /* Center horizontally */
+      align-items: center; /* Center vertically */
+  " onmouseover="this.style.backgroundColor='#D0D5DD'" onmouseout="this.style.backgroundColor='white'">
+  
+  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="4vw" height="3vh" viewBox="0 0 48 48">
+    <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path><path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path><path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path><path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
+    </svg>
+    <p style = "position: relative; right: 0.8vw;" >
+      Sign Up With Google
+    </p>
+    
+  </button>
+</form>
  </div>
  
   
@@ -439,13 +477,13 @@
          <div style="
          color: #667085;
          width: 50%; padding: 10px;
-         font-size: 12px;
+         font-size: calc(0.52vw + 0.5vh);
  font-family: 'Inter', sans-serif;
  font-weight: 400;
  line-height: 20px;
  letter-spacing: 0%;
- position: relative;
- left: 20vh;
+ position: absolute;
+ left: 10.5vw;
          ">
            Already have an account?
          </div>
@@ -454,13 +492,13 @@
          <div style="
          color: #175CD3;
          width: 50%; padding: 10px;
-         font-size: 12px;
+         font-size: calc(0.52vw + 0.6vh);
  font-family: 'Inter', sans-serif;
  font-weight: 600;
  line-height: 20px;
  letter-spacing: 0%;
- position: relative;
- left:7.75vh;
+ position: absolute;
+ left:20vw;
  cursor: pointer;"
  on:click={handleLogInClick}>
            Log In
