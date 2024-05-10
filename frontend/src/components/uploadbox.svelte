553b589c1a594cd88f3f9c0ed53a94667161777e
benjicherian <bche0062@student.monash.edu>
2024-05-10 23:00:52 +1000

    updating upload component

--- frontend/src/components/uploadbox.svelte
+++ frontend/src/components/uploadbox.svelte
@@ -1,17 +1,18 @@
-<!-- Upload Box Component
+<!--
 
-    Contains upload box functionaliy and styling
+    Upload Audio Box Component
 
-    Author: zwan0318
-    Edited by: bche0062
-    Last modified: 10/05/2024
+    Authors: Parul Garg (pgar0011)
+    Editied by:
+    Last Modified: 10/05/24
 
- -->
-
-<!--  -->
+-->
 
 <script>
-  import Dropzone from "svelte-file-dropzone";
+
+    import micIcon from "../assets/mic.png"
+
+    import Dropzone from "svelte-file-dropzone";
 
   const MAX_DURATION_SECONDS = 7200; // 7200 seconds = 120 minutes
 
@@ -45,14 +46,23 @@
       errorMessage = 'Invalid audio format! Your meeting audio must be in MP3 or WAV format';
     }
   }
+
 </script>
 
-<Dropzone on:drop={handleFilesSelect} accept=".mp3, .wav" class="hi">
-  <div class="dropzone-label">
-    <p>Upload meeting audio</p>
-    <p>Must be under 120 minutes. MP3 or WAV formats accepted.</p>
-  </div>
-</Dropzone>
+<div class="upload-box">
+
+    <label for="uploadAudioBox" class="custom-input">
+        <img class="mic-icon" src={micIcon} alt="Mic Icon" />
+        <span class="first-line">Upload meeting audio</span>
+        <span class="second-line">Must be under 120 minutes. MP3 or WAV formats accepted.</span>
+    
+    </label>
+
+    <input type="file" class = "hidden-box" id="uploadAudioBox" name="uploadAudioBox" accept="audio/mp3, audio/wav">
+</div>
+
+
+<Dropzone on:drop={handleFilesSelect} accept=".mp3, .wav" class="uploadDropZone"></Dropzone>
 
 {#if file}
   <p>File ready: {file.name} - {Math.floor(file.size / 1024)} KB</p>
@@ -60,3 +70,115 @@
   <p>{errorMessage}</p>
 {/if}
 
+
+<style>
+
+.upload-box{
+    display: flex;
+    justify-content: center;
+    
+}
+
+.hidden-box{
+    display: none;
+}
+
+.mic-icon{
+    /* mic */
+
+    width: 49px;
+    height: 55px;
+
+
+    /* Inside auto layout */
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+
+}
+
+.custom-input{
+
+    /* Upload Audio Box */
+
+    box-sizing: border-box;
+    
+
+    /* Auto layout */
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 24px 0px;
+    gap: 16px;
+
+    width: 800px;
+    height: 195px;
+
+    /* Blue/25 */
+    background: #F5FAFF;
+    /* Blue/300 */
+    border: 3px solid #84CAFF;
+    border-radius: 5px;
+
+    /* Inside auto layout */
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+
+}
+
+.first-line{
+
+    /* Upload meeting audio */
+
+    width: 250px;
+    height: 30px;
+
+    /* Text xl/Medium */
+    font-style: normal;
+    //font-family: "Inter";
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 30px;
+    /* identical to box height, or 150% */
+    text-align: center;
+
+    /* Blue/800 */
+    color: #1849A9;
+
+
+    /* Inside auto layout */
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+
+
+}
+
+.second-line{
+    /* Must be under 120 minutes. MP3 or WAV formats accepted. */
+
+    width: 230px;
+    height: 24px;
+
+    /* Text md/Regular */
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+    /* or 150% */
+    text-align: center;
+
+    /* Gray/400 */
+    color: #98A2B3;
+
+
+    /* Inside auto layout */
+    flex: none;
+    order: 1;
+    flex-grow: 0;
+
+}
+
+</style>
