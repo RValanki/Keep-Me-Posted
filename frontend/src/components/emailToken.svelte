21420cc5f861bd0cb6d69b895f0a761d88002f9a
Ayesha Tariq <atar0014@student.monash.edu>
2024-05-13 02:21:44 +1000

    finalised email token and remove button shows

--- frontend/src/components/emailToken.svelte
+++ frontend/src/components/emailToken.svelte
@@ -1,38 +1,44 @@
-<!--Email Token Component -->
+<!--Email Token Component 
 
+Contains email token functionality and styling
+
+Author: Ayesha Tariq
+Last Modified: 12/05/2024 -->
 <script>
     import Button from './button.svelte';
-
-    function deleteButtonClick(){
-        
-    }
-
-</script>
-
-<style>
-
-    @import url("https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0");
-
-    .token-body{
-        display: inline-flex;
-        align-items: center;
-        border-radius: 30px;
-        background-color: antiquewhite;
-        padding: 5px 10px;
-        color:black;
-        margin-right: 10px;
-    }
-
-    .token-body-span span {
-        margin-right:5px;
-    }
-</style>
-
-<Button>
+    // import removeUser from '../assets/remove-user-icon.png';
     
-    handleClick = {deleteButtonClick}
-    fullWidth={true}
+    function handleClick(){
+        console.log("Delete button clicked!");
 
-    <span class = "material-symbols-outlined">close</span>
+    }
 
-</Button>
+    import { Style } from '@material-ui/icons';
+    let inputText = '';
+</script>
+  
+  <style>
+    .email-token {
+    width: max-content; /* Adjust as needed */
+    height: 2px; /* Adjust as needed */
+    border-radius: 50px; /* Half of height to create the oval shape */
+    background-color:rgba(211, 211, 211, 0.236);
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border: 2px solid #dddddd4d; /* Optional: Add border */
+
+    }
+
+
+  </style>
+  
+  <div class="email-token">
+    {inputText}
+
+    <!-- <Button type="primary" on:click = {handleClick} text=""><img class="remove-user-icon" src={removeUser} alt="remove-user-icon"/></Button> -->
+    <button on:click={handleClick} style="border-radius:50px; color:thistle; border-color:gainsboro;cursor:pointer">X</button>
+  </div>
+
+  
