adfff546b06af082ebe662e545c8aeba271f146f
Rohit Valanki <rval00008@student.monash.edu>
2024-05-13 13:47:52 +1000

    Fixed email merge conflict issue

--- frontend/.svelte-kit/generated/client/app.js
+++ frontend/.svelte-kit/generated/client/app.js
@@ -7,7 +7,8 @@ export const nodes = [
 	() => import('./nodes/3'),
 	() => import('./nodes/4'),
 	() => import('./nodes/5'),
-	() => import('./nodes/6')
+	() => import('./nodes/6'),
+	() => import('./nodes/7')
 ];
 
 export const server_loads = [];
@@ -17,7 +18,8 @@ export const dictionary = {
 		"/email": [3],
 		"/login": [~4],
 		"/signup": [~5],
-		"/upload-audio": [6]
+		"/summary": [6],
+		"/upload-audio": [7]
 	};
 
 export const hooks = {
