fcd5ae5eba30a9df6aaa25a9f84b6569beab6101
zwan0318 <zwan0318@student.monash.edu>
2024-05-16 12:38:02 +1000

    Adjusted urls and confirmed email functioning

--- frontend/src/api-functions/simple_pathway.js
+++ frontend/src/api-functions/simple_pathway.js
@@ -4,13 +4,26 @@
 
 import { transcribe_audio } from "./transcribe-audio"
 import { updateStatus } from "../stores/simple-pathway-store"
+import { send_summary } from "./send_summary"
+import { send_email } from "./send_email"
+import { ContactsStore } from "../stores/contacts-store"
+
+let text = " Speaker A: Smoke from hundreds of wildfires in Canada is triggering air quality alerts throughout the US. Skylines from Maine to Maryland to Minnesota are gray and smoggy. And in some places, the air quality warnings include the warning to stay inside. We wanted to better understand what's happening here and why. So he called Peter DiCarlo, an associate professor in the department of Environmental Health and Engineering at Johns Hopkins University. Good morning. Professor. Speaker B: Good morning. g us and sharing this expertise with us. Speaker B: Thank you for having me. "
 
 export let simple_pathway = async (audioFile, baseURL) => {
     updateStatus("Transcribe")
     await transcribe_audio(audioFile, baseURL)
+
     updateStatus("Summary")
-    await transcribe_audio(audioFile, baseURL) //Placeholder
+    await send_summary(text, baseURL)
+
     updateStatus("Email")
-    await transcribe_audio(audioFile, baseURL) //Placeholder
+    let contacts = []
+    const unsubscribe = ContactsStore.subscribe(value => {
+        contacts = value;
+      });    
+    console.log(contacts)
+    await send_email(text, "bob", contacts, baseURL)
+
     updateStatus("Complete")
 }
