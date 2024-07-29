e88513328efaba23d2543f818109a9fb16b60940
Ayesha Tariq <atar0014@student.monash.edu>
2024-07-29 12:31:25 +1000

    one test case for smtp for confirmation of email being sent successfully has passed the test within VS terminal. Working on the 2nd testcase.

--- backend/kmp_backend/emailing/tests.py
+++ backend/kmp_backend/emailing/tests.py
@@ -1,13 +1,13 @@
 from django.test import TestCase
-from django.http import HttpRequest
-from views.py import send_email
+from django.http import HttpRequest, JsonResponse
 from unittest.mock import patch, MagicMock
+from emailing.views import send_email
+import json
 
 # Create your tests here.
 class TestEmailSent(TestCase):
-    
-    @patch('views.py.send_email')
 
+    @patch('emailing.views.send_email')
     def test_sending_email(self, mock_send_smtp_email):
         request = HttpRequest()
         request.method = 'POST'
@@ -18,11 +18,32 @@ class TestEmailSent(TestCase):
         }
 
         mock_response = JsonResponse({'details':'Emails sent successfully!'})
-        mock_send_smpty_email.return_value = mock_response
+       
+        mock_send_smtp_email.return_value = mock_response
 
         response = send_email(request)
+        response_content = json.loads(response.content.decode('utf-8'))
 
-        self.assertEqual(response.json(), {'details':'Emails sent succesfully!'})
+        self.assertEqual(response_content, {'details':'Emails sent successfully!'})
 
+    #draft testing below for status code response
 
+    # @patch('views.send_email')
+    # def test_sent_email_status(self, mock_status_email):
+    #     request = HttpRequest()
+    #     request.method = 'POST'
+    #     request.POST ={
+    #         'message': 'hi, please find the meeting minutes below',
+    #         'subject' : 'TestMeeting'
+    #         'contacts': 'tarayesha508@gmail.com',
+           
+    #     }
+        
+    #     mock_response = STATUS CODE 200
+    #     mock_status_email.return_value = mock_response
 
+    #     response = send_email(request)
+
+    #     self.assertEqual(response.json(), {'details':'Status code 200?'})
+
+        
