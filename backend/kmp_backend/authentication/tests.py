65fc8f0374c633f159b07ad52886ff4e970c6c7f
Marcus Wei <mwei0018@student.monash.edu>
2024-07-29 22:17:51 +1000

    test cases added for missing password and missing username for google authentication registration

--- backend/kmp_backend/authentication/tests.py
+++ backend/kmp_backend/authentication/tests.py
@@ -65,3 +65,21 @@ class AuthTests(TestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn('email', response.data)
+
+        #Test for missing username 
+        data = {
+            'email': 'invalid@example.com',
+            'password': 'invalidpassword123'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('username', response.data)
+
+        #Test for missing password 
+        data = {
+            'username': 'invaliduser2',
+            'email': 'invalid2@example.com',
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('password', response.data)
