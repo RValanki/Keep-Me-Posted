7db91a88c6ff71282edd0cdd3c0c2c96f3c5ffbd
Ayesha Tariq <atar0014@student.monash.edu>
2024-07-29 12:41:27 +1000

    Added docstring and comments to specify how the testcase works

--- backend/kmp_backend/emailing/tests.py
+++ backend/kmp_backend/emailing/tests.py
@@ -9,21 +9,29 @@ class TestEmailSent(TestCase):
 
     @patch('emailing.views.send_email')
     def test_sending_email(self, mock_send_smtp_email):
+        """
+        This function is responsible for testing if an email is sent successfully by determining the json response
+        received from the request. 
+        The function utilises mocking concept to test this functionality. @Patch is used to create a mock object
+        for the function send_email to be tested
+        """
         request = HttpRequest()
         request.method = 'POST'
+        #creating mock request
         request.POST = {
             'message': 'Here are the meeting minutes',
             'subject': 'TeamMeeting-20/07/2024',
             'contacts': 'tarayesha508@gmail.com'
         }
-
+        #setting the mock_response to what is expected
         mock_response = JsonResponse({'details':'Emails sent successfully!'})
        
         mock_send_smtp_email.return_value = mock_response
 
         response = send_email(request)
+        #extracting the response received from the function
         response_content = json.loads(response.content.decode('utf-8'))
-
+        
         self.assertEqual(response_content, {'details':'Emails sent successfully!'})
 
     #draft testing below for status code response
