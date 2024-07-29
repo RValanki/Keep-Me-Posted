83042b01718bf8fee027f3cfcdca9c877d5341dc
afia-farzana <afar0035@student.monash.edu>
2024-07-29 15:19:48 +1000

    Edited test cases to have dialect parameter + made several speaker audio generation method more flexible

--- backend/kmp_backend/assemblyaimodule/tests.py
+++ backend/kmp_backend/assemblyaimodule/tests.py
@@ -5,8 +5,8 @@ from .assemblyAI_module import transcribe, transcribe_with_speakers
 
 class TranscribeTestCase(TestCase):
     # Helper function to generate audio files in memory
-    def generateAudio(self, text: str, lang: str, accent: str):
-        tts = gTTS(text, lang, accent)
+    def generateAudio(self, text: str, tld: str, lang: str):
+        tts = gTTS(text, tld, lang)
         audio_file = io.BytesIO()
         tts.write_to_fp(audio_file)
         audio_file.seek(0)
@@ -17,29 +17,44 @@ class TranscribeTestCase(TestCase):
         test_text = "Hello, world."
         test_lang = "en"
         test_accent = "com.au"
-        test_audio = self.generateAudio(text=test_text, lang=test_lang, tld=test_accent)
+        test_audio = self.generateAudio(text=test_text, tld=test_accent, lang=test_lang)
         self.assertEqual(test_text, transcribe(test_audio))
     
     # Helper function to generate audio with multiple speakers 
-    def generateAudioMultipleSpeakers(self):
-        # Generating lines of audio with speakers of Indian and South African accented English 
-        line_one = self.generateAudio(text="This is a fascinating meeting that we are having today.", lang="en", tld="co.in")
-        line_two = self.generateAudio(text="Truly a most fascinating meeting.", lang="en", tld="co.za")
-        line_three = self.generateAudio(text="I have never in my life attended a more interesting meeting.", lang="en", tld="co.in")
-        line_four = self.generateAudio(text="I never have and never will.", lang="en", tld="co.za")
-        # Combining the lines of audio to create one audio file 
-        combined_audio = line_one + line_two + line_three + line_four
-        return combined_audio
+    def generateAudioMultipleSpeakers(self, conversation: list[str]):
+        transcript = []
+        # Setting the language 
+        test_lang = "en"
+        for line_num in range(len(conversation)):
+            # Setting and interchanging the accent 
+            test_tld = "co.za"
+            if line_num % 2 == 0:
+                test_tld = "co.in"
+            # Generating the conversation 
+            line = gTTS(text=conversation[line_num], tld=test_tld, lang=test_lang)
+            transcript.append(line)
+        # Writing the conversation to an audio file 
+        audio_file = io.BytesIO()
+        for line in range(len(transcript)):
+            transcript[line].write_to_fp(audio_file)
+        audio_file.seek(0)
+        return audio_file
+
 
     # Test 2: Test if transcribe_with_speakers() can transcribe text with speakers from a basic audio file 
     def test_transcribe_with_speakers(self):
-        test_audio = self.generateAudioMultipleSpeakers()
+        test_audio = self.generateAudioMultipleSpeakers([
+            "This is a fascinating meeting that we are having today.", 
+            "Truly a most fascinating meeting.",  
+            "I have never in my life attended a more interesting meeting.", 
+            "I never have and never will."
+        ])
 
         expected_result = (
-            "Speaker 1: This is a fascinating meeting that we are having today.\n"
-            "Speaker 2: Truly a most fascinating meeting.\n"
-            "Speaker 1: I have never in my life attended a more interesting meeting.\n"
-            "Speaker 2: I never have and never will.\n"
+            "Speaker A: This is a fascinating meeting that we are having today.\n" + 
+            "Speaker B: Truly a most fascinating meeting.\n" + 
+            "Speaker A: I have never in my life attended a more interesting meeting.\n" + 
+            "Speaker B: I never have and never will.\n"
         )
         
         self.assertEqual(expected_result, transcribe_with_speakers(test_audio))
