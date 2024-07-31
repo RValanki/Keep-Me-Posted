2f056ad082173b7f98930ce66a34494881e01b7b
Ironrohitman <111042421+Ironrohitman@users.noreply.github.com>
2024-07-31 12:05:22 +1000

    Updated mobile view scaling for the three distinct components on login page

--- frontend/src/components/landingPageTitle.svelte
+++ frontend/src/components/landingPageTitle.svelte
@@ -3,10 +3,10 @@ export let title = "Title";
 export let subTitle = "Subtitle!"
 </script>
   
-<div class="text-5xl font-inter font-bold mb-4 text-white">
+<div class="text-3xl sm:text-5xl font-inter font-bold mb-0 sm:mb-4 text-white">
     {title}
 </div>
-<div class="text-2xl font-inter font-thin italic text-white">
+<div class="text-xl sm:text-2xl font-inter font-thin italic text-white">
     {subTitle}
 </div>
   
--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -3,6 +3,8 @@
   import { onMount } from "svelte";
   import { goto } from "$app/navigation";
   import { updateAuth } from '../../stores/auth-store.js';
+  import Logo from "../../components/logo.svelte"
+  import LandingPageTitle from "../../components/landingPageTitle.svelte";
 
   let email = "";
   let password = "";
@@ -97,135 +99,27 @@
 
 </script>
 
-<html lang="en">
-   <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-   </head>
-   <body>
-    <div class = "login-container">
-      <div class = "kmp-container">
-         <div class = "logo-container">
-            <img src={logo} alt="Logo" class = "logo-style" />
-         </div>
-         <div class = "title">
-            <h1>Keep Me Posted</h1>
-         </div>
-         <div class="sub-title">
-            Meetings to summaries.
-         </div>
+ 
+<div class="login-container relative h-screen">
+   <div class="flex flex-col items-start absolute left-0 ml-8 sm:ml-40 top-0 sm:top-64">
+      <div class = "w-1/2 sm:w-full mt-6 sm:mt-0">
+         <Logo />
       </div>
-      <!-- svelte-ignore a11y-no-static-element-interactions -->
-      <div class="log-in-card">
-         <div class = "heading">
-            Log in to your account
-         </div>
-         <div class="subheading">
-            Welcome back! Please enter your details.
-         </div>
-         <div class = "email-container">
-            <div class="email-title">
-               Email
-            </div>
-            <div class="email-input-container">
-               <input class = "email-input-style"
-                  type="text"
-                  bind:value={email}
-                  placeholder="name@email.com"
-                  />
-            </div>
-            <div class="email-validation">
-               {emailValidationString}
-            </div>
-         </div>
-         <div class="password-container">
-            <div class = "password-title">
-               Password
-            </div>
-            <div class = "password-input-container">
-               <input class = "password-input-style"
-                  type="password"
-                  bind:value={password}
-                  placeholder="••••••••"
-                  />
-            </div>
-            <div class = "password-validation">
-               {passwordValidationString}
-            </div>
-         </div>
-         <!-- svelte-ignore a11y-click-events-have-key-events -->
-         <div class = "forgot-password"
-            onmouseover="this.style.color='#6941C6'"
-            onmouseout="this.style.color='#667085'"
-            on:click={() => {
-            alert("Womp Womp");
-            }}
-            >
-            Forgot Password?
-         </div>
-         <div class = "primary-button-container">
-            <button
-               class = "primary-button"
-               on:click={handleSignIn}
-               onmouseover="this.style.backgroundColor='#0c4eae'"
-               onmouseout="this.style.backgroundColor='#1570EF'"
-               >
-            Sign In
-            </button>
-         </div>
-         <div class = "secondary-button-container">
-            <form method="post" action="?/OAuth2">
-               <button
-                  class = "secondary-button"
-                  alt="google sign in"
-                  onmouseover="this.style.backgroundColor='#D0D5DD'"
-                  onmouseout="this.style.backgroundColor='white'"
-                  >
-                  <svg
-                     xmlns="http://www.w3.org/2000/svg"
-                     x="0px"
-                     y="0px"
-                     width="4vw"
-                     height="3vh"
-                     viewBox="0 0 48 48"
-                     >
-                     <path
-                        fill="#FFC107"
-                        d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
-                        ></path>
-                     <path
-                        fill="#FF3D00"
-                        d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
-                        ></path>
-                     <path
-                        fill="#4CAF50"
-                        d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
-                        ></path>
-                     <path
-                        fill="#1976D2"
-                        d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
-                        ></path>
-                  </svg>
-                  <p class = "secondary-button-text-style">Sign In With Google</p>
-               </button>
-            </form>
-         </div>
-         <div class = "sign-up-container">
-            <div class = "sign-up-label">
-               Don't have an account?
-            </div>
-            <!-- svelte-ignore a11y-click-events-have-key-events -->
-            <!-- svelte-ignore a11y-no-static-element-interactions -->
-            <div class = "sign-up-link"
-               on:click={handleSignUpClick}
-               >
-               Sign Up
-            </div>
-         </div>
+      <div class = "absolute top-0 sm:top-40 ml-24 sm:ml-0 flex flex-col mt-8 sm:mt-0 w-[500px]">
+         <LandingPageTitle title="Keep Me Posted" subTitle="Meetings to summaries." />
       </div>
-    </div>
-   </body>
-</html>
+   </div>
+   
+   <div class="absolute bottom-0 sm:bottom-20 right-0 w-full sm:w-2/5 h-4/5 bg-white mr-0 sm:mr-12 rounded-lg rounded-b-none sm:rounded-b-lg">
+      <!-- Your content here -->
+   </div>
+   
+</div>
+
+
+
+
+ 
 
 <!-- this styling secton contains styles for many components used throughout the page-->
 <style>
