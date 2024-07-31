9539f1ca727ebb8201ebe10d5083a059a1580104
Harrison Lane <hlan0016@student.monash.edu>
2024-07-31 21:18:44 +1000

    fixed issue with email token font scaling

--- frontend/src/components/emailList.svelte
+++ frontend/src/components/emailList.svelte
@@ -8,8 +8,8 @@
 
 <div class="flex py-8 flex-wrap justify-center self-center max-w-xl gap-2">
   {#each $ContactsStore as email}
-    <div class="flex align-middle p-2 bg-[#F8F9FC] rounded-md">
-      <div>{email}</div>
+    <div class="flex align-middle p-2 gap-1 bg-[#F8F9FC] rounded-md">
+      <div class="text-xs">{email}</div>
       <button on:click={remove_email(email)}
         ><img
           class="h-4"
