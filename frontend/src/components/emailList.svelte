d3cfdfddbc346ec7c626c7684f6e320b187c80ec
Harrison Lane <hlan0016@student.monash.edu>
2024-07-31 21:13:03 +1000

    cleaning up unused classes

--- frontend/src/components/emailList.svelte
+++ frontend/src/components/emailList.svelte
@@ -1,13 +1,5 @@
 <script>
   import { ContactsStore } from "../stores/contacts-store"
-  import { onMount } from "svelte";
-
-onMount(() => {
-    ContactsStore.update((prev) => [])
-    for(let i = 0; i < 15; i++) {
-        ContactsStore.update((prev) => [...prev, "111" * i + "@gmail.com"]);
-    }
-})
 
   let remove_email = (email) => {
     ContactsStore.update((prev) => prev.filter((contact) => contact != email));
@@ -17,7 +9,7 @@ onMount(() => {
 <div class="flex py-8 flex-wrap justify-center self-center max-w-xl gap-2">
   {#each $ContactsStore as email}
     <div class="flex align-middle p-2 bg-[#F8F9FC] rounded-md">
-      <div class="token">{email}</div>
+      <div>{email}</div>
       <button on:click={remove_email(email)}
         ><img
           class="h-4"
