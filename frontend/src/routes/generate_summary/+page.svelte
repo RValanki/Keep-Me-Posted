f6aec0a816441c00fc8bccb39e730cc618c39c7e
bdan0005 <bdan0005@student.monash.edu>
2024-08-01 20:36:55 +1000

    Initial push for summary generation page

--- frontend/src/routes/generate_summary/+page.svelte
+++ frontend/src/routes/generate_summary/+page.svelte
@@ -9,6 +9,36 @@
 <script>
     import TopBar from "../../components/topbar.svelte";
     import Button from "../../components/button.svelte";
+    import SummaryBox from "../../components/summary-box.svelte";
     import { goto } from "$app/navigation";
 
+    export let title = "Your Summary is Being Generated...";
+    export let subTitle = "We are still generating your summary...";
+
+    let backBtn = () => {
+        goto("/upload-audio");
+    };
+
+    let forwardBtn = () => {
+        goto("/email");
+    };
+
 </script>
+
+<style>
+
+</style>
+
+<body>
+    <TopBar />
+
+    <div class="text-5xl font-inter font-bold mb-4 text-black flex flex-col justify-center items-center">
+        {title}
+    </div>
+    <div class="text-2xl font-inter font-thin text-black flex flex-col justify-center items-center">
+        {subTitle}
+    </div>
+
+    <SummaryBox />
+
+</body>
