75c71dd76fded8fcff427ef14ff2cd7c213cd4be
bdan0005 <bdan0005@student.monash.edu>
2024-08-01 20:19:58 +1000

    Header for new summary box component

---
+++ frontend/src/components/summary-box.svelte
@@ -0,0 +1,13 @@
+<!-- Summary box component
+
+    Contains the summary generated and the subject
+
+    Author: Diya Ramesh, Brenda Dang
+    Last modified: 1/08/2024
+
+-->
+
+<!--  -->
+<script>
+    
+</script>
