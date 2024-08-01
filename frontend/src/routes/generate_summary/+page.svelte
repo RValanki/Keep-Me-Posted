392421fdc938056969303a0ca37191a77f8d4333
bdan0005 <bdan0005@student.monash.edu>
2024-08-01 20:18:11 +1000

    Write header for summary generation page

---
+++ frontend/src/routes/generate_summary/+page.svelte
@@ -0,0 +1,14 @@
+<!-- Summary generation page
+
+    Page that shows the summary generated
+
+    Author:Brenda Dang, Diya Ramesh
+    Last Modified: 1/08/2024
+-->
+
+<script>
+    import TopBar from "../../components/topbar.svelte";
+    import Button from "../../components/button.svelte";
+    import { goto } from "$app/navigation";
+
+</script>
