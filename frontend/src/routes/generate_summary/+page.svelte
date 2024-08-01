9d491f702d970a148f9cf29fb04888fce547c833
bdan0005 <bdan0005@student.monash.edu>
2024-08-01 20:55:59 +1000

    Created summary generation page

--- frontend/src/routes/generate_summary/+page.svelte
+++ frontend/src/routes/generate_summary/+page.svelte
@@ -32,13 +32,33 @@
 <body>
     <TopBar />
 
-    <div class="text-5xl font-inter font-bold mb-4 text-black flex flex-col justify-center items-center">
+    <div class="text-4xl font-inter font-bold mb-4 text-black flex flex-col justify-center items-center mt-20">
         {title}
     </div>
-    <div class="text-2xl font-inter font-thin text-black flex flex-col justify-center items-center">
+    <div class="text-xl font-inter font-thin text-black flex flex-col justify-center items-center mt-6">
         {subTitle}
     </div>
 
-    <SummaryBox />
+    <div class="mt-10">
+        <SummaryBox />
+    </div>
+
+    <div class="flex flex-row my-10 w-full">
+        <div class="ml-3">
+            <Button 
+            class="primary" 
+            text="Re-Upload Audio"
+            icon='../src/assets/arrow-left.png'
+            handleClick={backBtn}/>
+        </div>
+
+        <div class="mr-3 ml-auto flex justify-end">
+            <Button 
+            class="primary" 
+            text="Add Recipients"
+            icon='../src/assets/arrow-right.png'
+            handleClick={forwardBtn}/>
+        </div>
+    </div>
 
 </body>
