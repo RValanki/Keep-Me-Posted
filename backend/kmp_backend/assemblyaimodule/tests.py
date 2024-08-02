2c1386db262308e2e45c8f5fc0d563c057a42774
ZWAN0318 <zwan0318@student.monash.edu>
2024-08-02 21:02:11 +1000

    Created mock test of successful transcription

--- backend/kmp_backend/assemblyaimodule/tests.py
+++ backend/kmp_backend/assemblyaimodule/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from gtts import gTTS
 from io import BytesIO
 from .assemblyAI_module import transcribe, transcribe_with_speakers
+import unittest
+from unittest.mock import patch
 
 class TranscribeTestCase(TestCase):
     @staticmethod
@@ -17,6 +19,23 @@ class TranscribeTestCase(TestCase):
 
         return audio_file
     
+    # Test 1: Test transcribe() success
+    @patch('aai.Transcriber')
+    def test_transcribe_success(self, MockAssemblyAI):
+        # Create a mock AssemblyAI instance and a mock transcript object
+        mock_transcriber = MockAssemblyAI.return_value
+        mock_transcript = MagicMock()
+
+        # Set the transcript status and text for a successful transcription
+        mock_transcript.status = aai.TranscriptStatus.success
+        test_text = "Testing transcription function"
+        mock_transcript.text = test_text
+        mock_transcribe.transcribe.return_value = mock_transcript
+        
+        # Call transcribe with mocked transcript and assert
+        result = transcribe('hello.mp3')
+        self.assertEqual(result, test_text)
+
     # Test 1: Test return type of transcribe()
     def test_transcribe_type(self):
         test_text = "Hello, world."
