6c1cfe62edfc006337af57dae02cf032b89bc718
Ironrohitman <111042421+Ironrohitman@users.noreply.github.com>
2024-08-04 16:40:25 +1000

    Updated tertiary button for button component, and integrated button components to login page

--- frontend/src/components/button.svelte
+++ frontend/src/components/button.svelte
@@ -11,30 +11,37 @@
     export let type = 'primary';
     export let handleClick = () => {};
     export let icon = ''; // Default empty string for icon URL
+    export let iconSvg = ''; // Default empty string for inline SVG
     export let text = 'Button';
-	export let fullWidth = false; // Default to false for not full width
+    export let fullWidth = false; // Default to false for not full width
+    export let fitContainerHeight = false; // New prop to fit container height
 
     // Internal handleClick function that calls the passed handleClick prop
     function internalHandleClick() {
         handleClick();
     }
 </script>
-<style>
-</style>
 
-<div class="flex justify-center sm:justify-start">
+<div class="flex justify-center sm:justify-start h-full">
     <button 
-        class="flex items-center justify-center font-sans border-none rounded-lg px-3 py-2 md:px-4 md:py-3 lg:px-5 lg:py-3.5 transition ease-in-out duration-300 min-h-11
-        {type === 'primary' ? 'bg-blue-600 text-white hover:bg-blue-800' : ''}
-        {type === 'secondary' ? 'bg-blue-50 text-blue-700 hover:bg-blue-100' : ''}
-        {type === 'tertiary' ? 'bg-white text-gray-800 border border-gray-300' : ''}
-        {type === 'disabled' ? 'bg-gray-200 text-white cursor-not-allowed' : ''}
-        {type === 'error' ? 'bg-red-600 text-white' : ''}
-        {fullWidth ? 'w-full text-center' : ''}"
+        class="flex items-center justify-center font-sans rounded-lg px-3 py-2 md:px-4 md:py-3 lg:px-5 lg:py-3.5 transition ease-in-out duration-300 min-h-11
+        {type === 'primary' ? 'bg-blue-600 text-white hover:bg-blue-800 border-none' : ''}
+        {type === 'secondary' ? 'bg-blue-50 text-blue-700 hover:bg-blue-100 border-none' : ''}
+        {type === 'tertiary' ? 'bg-white text-gray-800 border-[1px] border-gray-300 hover:bg-gray-300' : ''}
+        {type === 'disabled' ? 'bg-gray-200 text-white cursor-not-allowed border-none' : ''}
+        {type === 'error' ? 'bg-red-600 text-white border-none' : ''}
+        {fullWidth ? 'w-full text-center' : ''}
+        {fitContainerHeight ? 'h-full' : ''}"
         on:click="{internalHandleClick}">
         {#if icon}
             <!-- svelte-ignore a11y-missing-attribute -->
             <img src="{icon}" class="mr-2 max-w-4 md:max-w-5 lg:max-w-6">
+        {:else if iconSvg}
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <div src="{icon}" class=" mr-2 md:mr-6 max-w-6">
+                {@html iconSvg}
+            </div>
+            
         {/if}
 
         {text}
--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -7,6 +7,36 @@
   import LandingPageTitle from "../../components/landingPageTitle.svelte";
   import InputFieldWithValidation from "../../components/input-field-with-validation.svelte";
   import LoginCardHeader from "../../components/loginCardHeader.svelte"
+  import Button from "../../components/button.svelte";
+  import LoginPrompt from "../../components/loginPrompt.svelte"
+   
+  let googleIcon = `<svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="4vw"
+              height="3vh"
+              viewBox="0 0 48 48"
+            >
+              <path
+                fill="#FFC107"
+                d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+              ></path>
+              <path
+                fill="#FF3D00"
+                d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+              ></path>
+              <path
+                fill="#4CAF50"
+                d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+              ></path>
+              <path
+                fill="#1976D2"
+                d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+              ></path>
+            </svg>`
+
+
 
   let email = "";
   let password = "";
@@ -123,15 +153,41 @@
    </div>
    <!-- Bottom Row on Mobile / Right Column on Desktop -->
    <div class="w-full md:w-1/2 flex justify-center items-center h-full">
-      <div class=" h-full w-full md:w-1/2 flex justify-center items-center">
+      <div class=" h-full w-full md:w-2/3 flex justify-center items-center">
 
-         <div class="p-16  h-full md:h-3/4 w-full sm:w-3/4 md:w-3/5 rounded-[16px]  bg-white  md:min-w-[450px] min-h-full md:min-h-[500px] ml-0 md:ml-16 mr-0 md:mr-16 ">
+         <div class="p-16  h-full md:h-3/4 w-full sm:w-3/4 md:w-4/5 rounded-[16px]  bg-white  md:min-w-[450px] min-h-full md:min-h-[550px]  ml-0 md:ml-16 mr-0 md:mr-16 ">
             <!-- Content here -->
             <div class = "w-full h-full">
                <LoginCardHeader heading = "Log in to your account" subheading = "Welcome back! Please enter your details."/>
                <InputFieldWithValidation label = "Email" type = "email" placeholder = "name@email.com" validationMessage = {emailValidationString}/>
                <InputFieldWithValidation label = "Password" type = "password" placeholder = "••••••••" validationMessage = {passwordValidationString}/>
+               
+               <div class="w-full mb-5 flex justify-end text-xs text-gray-500 font-bold hover:text-purple-500 cursor-pointer">
+                  Forgot Password
+              </div>
+              
+              
 
+               <div class = "w-full h-[20px] mb-9">
+                  <Button fullWidth = {true} type = "primary" text = "Sign In" handleClick = {handleSignIn}/>
+               </div>
+
+               <div>
+                  <form class = "w-full h-[20px] mb-10" method="post" action="?/OAuth2">
+                     <Button fullWidth = {true} type = "tertiary" text = "Sign in with Google" iconSvg = {googleIcon}/>
+                  </form>
+                  
+               </div>
+
+               <div class="w-full flex justify-center">
+                  <LoginPrompt text = "Don’t have an account?" linkText = "Sign Up" handleClick = {handleSignUpClick}/>
+              </div>
+              
+
+               
+               
+               
+               
                
             </div>
           </div>
