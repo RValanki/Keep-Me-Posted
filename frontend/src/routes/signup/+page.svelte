5962207213de45e76dedcb7062de2572c7be2435
Ironrohitman <111042421+Ironrohitman@users.noreply.github.com>
2024-08-05 17:36:57 +1000

    Formatted syntax for sign up page

--- frontend/src/routes/signup/+page.svelte
+++ frontend/src/routes/signup/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-
   import { onMount } from "svelte";
   import { goto } from "$app/navigation";
   import Logo from "../../components/logo.svelte";
@@ -8,7 +7,7 @@
   import LoginCardHeader from "../../components/loginCardHeader.svelte";
   import Button from "../../components/button.svelte";
   import LoginPrompt from "../../components/loginPrompt.svelte";
-  
+
   let googleIcon = `<svg
               xmlns="http://www.w3.org/2000/svg"
               x="0px"
@@ -154,99 +153,90 @@
   function handleLogInClick() {
     goto("/login");
   }
-
 </script>
 
 <div class="m-0 h-screen bg-gradient-to-l from-[#53b1fd] to-[#1570ef] flex justify-center items-center text-white">
   <div class="flex flex-col md:flex-row h-screen w-full">
-     <!-- Top Row on Mobile / Left Column on Desktop -->
-     <div class="flex md:flex-col w-full md:w-1/2">
-        <div class="ml-0 sm:ml-24 md:ml-0 w-1/4 md:w-full md:flex-1">
-           <div class=" ml-4 p-4 sm:p-6 md:p-0 mt-1 md:mt-0 min-w-[100%] md:min-w-[150px] ml-0 md:ml-40 h-full w-1/5 flex flex-col justify-end md:justify-end">
-              <Logo class="mt-auto" />
-           </div>
+    <!-- Top Row on Mobile / Left Column on Desktop -->
+    <div class="flex md:flex-col w-full md:w-1/2">
+      <div class="ml-0 sm:ml-24 md:ml-0 w-1/4 md:w-full md:flex-1">
+        <div class=" ml-4 p-4 sm:p-6 md:p-0 mt-1 md:mt-0 min-w-[100%] md:min-w-[150px] ml-0 md:ml-40 h-full w-1/5 flex flex-col justify-end md:justify-end">
+          <Logo class="mt-auto" />
         </div>
-        <div class="w-3/4 md:w-full md:flex-1 flex items-center md:items-start">
-           <div class="ml-4 md:ml-40 mt-2 md:mt-4 mb-0">
-              <LandingPageTitle
-                 title="Keep Me Posted"
-                 subTitle="Meetings to summaries."
+      </div>
+      <div class="w-3/4 md:w-full md:flex-1 flex items-center md:items-start">
+        <div class="ml-4 md:ml-40 mt-2 md:mt-4 mb-0">
+          <LandingPageTitle
+            title="Keep Me Posted"
+            subTitle="Meetings to summaries."
+          />
+        </div>
+      </div>
+    </div>
+    <!-- Bottom Row on Mobile / Right Column on Desktop -->
+    <div class="w-full md:w-1/2 flex justify-center items-center h-full">
+      <div class="h-full w-full md:w-full lg:w-2/3 flex justify-center items-center">
+        <div class="py-10 px-16 h-full md:h-3/4 lg:h-3/4 w-full sm:w-3/4 md:w-4/5 rounded-[16px] bg-white md:min-w-full lg:min-w-[450px] md:min-h-[635px] lg:min-h-[635px] ml-0 md:ml-16 mr-0 md:mr-16 max-h-[700px]">
+          <!-- Content here -->
+          <LoginCardHeader heading="Sign Up" subheading="Create an account!" />
+
+          <InputFieldWithValidation
+            label="Email"
+            placeholder="name@email.com"
+            bind:value={email}
+            validationMessage={emailValidationString}
+            validationActive={emailValidationActive}
+          />
+
+          <InputFieldWithValidation
+            label="Password"
+            isPasswordType={true}
+            placeholder="••••••••"
+            bind:value={password}
+            validationMessage={passwordValidationString}
+            validationActive={passwordValidationActive}
+            showValidation={true}
+          />
+
+          <InputFieldWithValidation
+            label="Verify Password"
+            isPasswordType={true}
+            placeholder="••••••••"
+            bind:value={verifyPassword}
+            validationMessage={verifyPasswordValidationString}
+            validationActive={verifyPasswordValidationActive}
+          />
+
+          <div class="w-full h-[20px] mb-9 mt-8">
+            <Button
+              fullWidth={true}
+              type="primary"
+              text="Sign Up"
+              handleClick={handleSignUp}
+            />
+          </div>
+
+          <div>
+            <form class="w-full h-[20px] mb-10" method="post" action="?/OAuth2">
+              <Button
+                fullWidth={true}
+                type="tertiary"
+                text="Sign Up with Google"
+                iconSvg={googleIcon}
               />
-           </div>
+            </form>
+          </div>
+
+          <div class="w-full flex justify-center">
+            <LoginPrompt
+              text="Already have an account?"
+              linkText="Log In"
+              isPrimary={false}
+              handleClick={handleLogInClick}
+            />
+          </div>
         </div>
-     </div>
-     <!-- Bottom Row on Mobile / Right Column on Desktop -->
-     <div class="w-full md:w-1/2 flex justify-center items-center h-full">
-        <div class="h-full w-full md:w-full lg:w-2/3 flex justify-center items-center">
-           <div class="py-10 px-16 h-full md:h-3/4 lg:h-3/4 w-full sm:w-3/4 md:w-4/5 rounded-[16px] bg-white md:min-w-full lg:min-w-[450px] md:min-h-[635px] lg:min-h-[635px] ml-0 md:ml-16 mr-0 md:mr-16 max-h-[700px]">
-              <!-- Content here -->
-              <LoginCardHeader heading = "Sign Up" subheading = "Create an account!"/>
-
-              <InputFieldWithValidation
-                     label="Email"
-                     placeholder="name@email.com"
-                     bind:value={email}
-                     validationMessage = {emailValidationString}
-                     validationActive = {emailValidationActive}
-                    
-                  />
-
-                  <InputFieldWithValidation
-                     label="Password"
-                     isPasswordType={true}
-                     placeholder="••••••••"
-                     bind:value={password}
-                     validationMessage = {passwordValidationString}
-                     validationActive = {passwordValidationActive}
-                     showValidation = {true}
-                  />
-
-                  <InputFieldWithValidation
-                     label="Verify Password"
-                     isPasswordType={true}
-                     placeholder="••••••••"
-                     bind:value={verifyPassword}
-                     validationMessage = {verifyPasswordValidationString}
-                     validationActive = {verifyPasswordValidationActive}
-                     
-                  />
-
-                  <div class="w-full h-[20px] mb-9 mt-8">
-                    <Button
-                       fullWidth={true}
-                       type="primary"
-                       text="Sign Up"
-                       handleClick={handleSignUp}
-                    
-                    />
-                 </div>
-
-                 <div>
-                    <form
-                       class="w-full h-[20px] mb-10"
-                       method="post"
-                       action="?/OAuth2"
-                    >
-                       <Button
-                          fullWidth={true}
-                          type="tertiary"
-                          text="Sign Up with Google"
-                          iconSvg={googleIcon}
-                       />
-                    </form>
-                 </div>
-
-                 <div class="w-full flex justify-center">
-                    <LoginPrompt
-                       text="Already have an account?"
-                       linkText="Log In"
-                       isPrimary = {false}
-                       handleClick={handleLogInClick}
-                      
-                    />
-                 </div>
-           </div>
-        </div>
-     </div>
+      </div>
+    </div>
   </div>
 </div>
