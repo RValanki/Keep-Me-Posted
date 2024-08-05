467df76309ee5a95d6ea45c666089bed6575f300
Ironrohitman <111042421+Ironrohitman@users.noreply.github.com>
2024-08-05 17:17:26 +1000

    Refactored sign up page to utilise tailwind and modular components

--- frontend/src/routes/signup/+page.svelte
+++ frontend/src/routes/signup/+page.svelte
@@ -1,15 +1,49 @@
 <script>
-  import logo from "../../assets/kmp-logo-large.png";
+
   import { onMount } from "svelte";
   import { goto } from "$app/navigation";
+  import Logo from "../../components/logo.svelte";
+  import LandingPageTitle from "../../components/landingPageTitle.svelte";
+  import InputFieldWithValidation from "../../components/input-field-with-validation.svelte";
+  import LoginCardHeader from "../../components/loginCardHeader.svelte";
+  import Button from "../../components/button.svelte";
+  import LoginPrompt from "../../components/loginPrompt.svelte";
+  
+  let googleIcon = `<svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="4vw"
+              height="3vh"
+              viewBox="0 0 48 48"
+            >
+              <path
+                fill="#FFC107"
+                d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+              ></path>
+              <path
+                fill="#FF3D00"
+                d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+              ></path>
+              <path
+                fill="#4CAF50"
+                d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+              ></path>
+              <path
+                fill="#1976D2"
+                d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+              ></path>
+            </svg>`;
 
   let email = "";
   let password = "";
   let verifyPassword = "";
   let emailValidationString = "";
   let passwordValidationString = "Password must contain at least 8 characters, capital letters, and a special character";
-  let passwordValidationStringColor = "#959595";
   let verifyPasswordValidationString = "";
+  let emailValidationActive = false;
+  let passwordValidationActive = false;
+  let verifyPasswordValidationActive = false;
 
   function isValidEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -25,22 +59,25 @@
   function emailValidation(email) {
     if (email == "") {
       emailValidationString = "Please Enter Email";
+      emailValidationActive = true;
       return false;
     } else if (!isValidEmail(email)) {
       emailValidationString = "Please Enter Valid Email";
+      emailValidationActive = true;
       return false;
     } else {
       emailValidationString = "";
+      emailValidationActive = false;
       return true;
     }
   }
 
   function passwordValidation(password) {
     if (password == "" || !isValidPassword(password)) {
-      passwordValidationStringColor = "red";
+      passwordValidationActive = true;
       return false;
     } else {
-      passwordValidationStringColor = "#959595";
+      passwordValidationActive = false;
       return true;
     }
   }
@@ -48,12 +85,15 @@
   function verifyPasswordValidation(password, verifyPassword) {
     if (verifyPassword == "") {
       verifyPasswordValidationString = "Please Enter Password";
+      verifyPasswordValidationActive = true;
       return false;
     } else if (verifyPassword != password) {
       verifyPasswordValidationString = "Password does not match";
+      verifyPasswordValidationActive = true;
       return false;
     } else {
       verifyPasswordValidationString = "";
+      verifyPasswordValidationActive = false;
       return true;
     }
   }
@@ -61,6 +101,7 @@
   function handleEmailInUse(response) {
     if ((response.error = "This email address is already in use")) {
       emailValidationString = response["error"];
+      emailValidationActive = true;
     }
   }
 
@@ -116,438 +157,96 @@
 
 </script>
 
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  </head>
-  <body>
-    <div class="kmp-container">
-      <div class="logo-container">
-        <img src={logo} alt="Logo" class="logo-style" />
-      </div>
-      <div class="title">
-        <h1>
-          Keep Me Posted
-        </h1>
-      </div>
-      <div class="sub-title">Meetings to summaries.</div>
-    </div>
-    <div class="sign-up-card">
-      <div class="heading">Sign Up</div>
-      <div class="subheading">Create an account!</div>
-      <div class="email-container">
-        <div class="email-title">Email</div>
-        <div class="email-input-container">
-          <input
-            class="email-input-style"
-            type="text"
-            bind:value={email}
-            placeholder="name@email.com"
-          />
+<div class="m-0 h-screen bg-gradient-to-l from-[#53b1fd] to-[#1570ef] flex justify-center items-center text-white">
+  <div class="flex flex-col md:flex-row h-screen w-full">
+     <!-- Top Row on Mobile / Left Column on Desktop -->
+     <div class="flex md:flex-col w-full md:w-1/2">
+        <div class="ml-0 sm:ml-24 md:ml-0 w-1/4 md:w-full md:flex-1">
+           <div class=" ml-4 p-4 sm:p-6 md:p-0 mt-1 md:mt-0 min-w-[100%] md:min-w-[150px] ml-0 md:ml-40 h-full w-1/5 flex flex-col justify-end md:justify-end">
+              <Logo class="mt-auto" />
+           </div>
         </div>
-        <div class="email-validation">
-          {emailValidationString}
+        <div class="w-3/4 md:w-full md:flex-1 flex items-center md:items-start">
+           <div class="ml-4 md:ml-40 mt-2 md:mt-4 mb-0">
+              <LandingPageTitle
+                 title="Keep Me Posted"
+                 subTitle="Meetings to summaries."
+              />
+           </div>
         </div>
-      </div>
-      <div class="password-container">
-        <div class="password-title">Password</div>
-        <div class="password-input-container">
-          <input
-            class="password-input-style"
-            type="password"
-            bind:value={password}
-            placeholder="••••••••"
-          />
-        </div>
-        <div
-          class="password-validation"
-          style="color: {passwordValidationStringColor};"
-        >
-          {passwordValidationString}
-        </div>
-      </div>
-      <div class="verify-password-container">
-        <div class="verify-password-title">Verify Password</div>
-        <div class="verify-password-input-container">
-          <input
-            class="verify-password-input-style"
-            type="password"
-            bind:value={verifyPassword}
-            placeholder="••••••••"
-          />
-        </div>
-        <div class="verify-password-validation">
-          {verifyPasswordValidationString}
-        </div>
-      </div>
-      <div class="primary-button-container">
-        <button
-          class="primary-button"
-          on:click={handleSignUp}
-          onmouseover="this.style.backgroundColor='#0c4eae'"
-          onmouseout="this.style.backgroundColor='#1570EF'"
-        >
-          Sign Up
-        </button>
-      </div>
-      <div class="secondary-button-container">
-        <form method="post" action="?/OAuth2">
-          <button
-            class="secondary-button"
-            alt="google sign in"
-            onmouseover="this.style.backgroundColor='#D0D5DD'"
-            onmouseout="this.style.backgroundColor='white'"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              x="0px"
-              y="0px"
-              width="4vw"
-              height="3vh"
-              viewBox="0 0 48 48"
-            >
-              <path
-                fill="#FFC107"
-                d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
-              ></path>
-              <path
-                fill="#FF3D00"
-                d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
-              ></path>
-              <path
-                fill="#4CAF50"
-                d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
-              ></path>
-              <path
-                fill="#1976D2"
-                d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
-              ></path>
-            </svg>
-            <p class="secondary-button-label">Sign Up With Google</p>
-          </button>
-        </form>
-      </div>
-      <!-- svelte-ignore a11y-no-static-element-interactions -->
-      <div class="log-in-container">
-        <div class="log-in-label">Already have an account?</div>
-        <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <div class="log-in-link" on:click={handleLogInClick}>Log In</div>
-      </div>
-    </div>
-  </body>
-</html>
+     </div>
+     <!-- Bottom Row on Mobile / Right Column on Desktop -->
+     <div class="w-full md:w-1/2 flex justify-center items-center h-full">
+        <div class="h-full w-full md:w-full lg:w-2/3 flex justify-center items-center">
+           <div class="py-10 px-16 h-full md:h-3/4 lg:h-3/4 w-full sm:w-3/4 md:w-4/5 rounded-[16px] bg-white md:min-w-full lg:min-w-[450px] md:min-h-[600px] lg:min-h-[600px] ml-0 md:ml-16 mr-0 md:mr-16 max-h-[700px]">
+              <!-- Content here -->
+              <LoginCardHeader heading = "Sign Up" subheading = "Create an account!"/>
 
-<!-- this styling secton contains styles for many components used throughout the page-->
-<style>
-  body {
-    margin: 0;
-    height: 100vh;
-    background: linear-gradient(to left, #53b1fd 0%, #1570ef 100%);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: white;
-  }
-  h1{
-    color: white;
-  }
-  .sign-up-card {
-    height: 75vh;
-    width: 32vw;
-    border-radius: 16px;
-    position: absolute;
-    right: 12vw;
-    background-color: white;
-  }
-  .logo-style {
-    width: 100%;
-    height: auto;
-  }
-  .sub-title {
-    font-size: calc(1vw + 0.9vh);
-    font-family: "Inter", sans-serif;
-    font-weight: 100;
-    font-style: italic;
-    letter-spacing: 0%;
-    position: relative;
-    top: 0.5vh;
-    color:white;
-  }
-  .title {
-    font-size: calc(2vw + 2.5vh);
-    font-family: "Inter", sans-serif;
-    font-weight: 550;
-    letter-spacing: 0%;
-    position: relative;
-    top: 0.2vh;
-  }
-  .logo-container {
-    max-width: 41%;
-  }
-  .kmp-container {
-    position: absolute;
-    left: 10vw;
-    top: 35vh;
-  }
-  .heading {
-    width: 60vh;
-    height: 10vh;
-    color: #101828;
-    position: absolute;
-    top: 6vh;
-    font-size: calc(1.5vw + 1vh);
-    font-family: "Inter", sans-serif;
-    font-weight: 575;
-    line-height: 38px;
-    letter-spacing: 0%;
-    vertical-align: baseline;
-    left: 5.8vw;
-  }
-  .subheading {
-    width: 60vh;
-    height: 10vh;
-    color: #667085;
-    position: absolute;
-    top: 11vh;
-    font-size: calc(0.5vw + 1vh);
-    font-family: "Inter", sans-serif;
-    font-weight: 350;
-    line-height: 24px;
-    letter-spacing: 0%;
-    vertical-align: baseline;
-    left: 5.8vw;
-  }
-  .email-container {
-    width: 60vh;
-    height: 10vh;
-    position: absolute;
-    top: 18vh;
-  }
-  .email-title {
-    font-size: calc(0.4vw + 1vh);
-    font-family: "Inter", sans-serif;
-    font-weight: 500;
-    line-height: 20px;
-    letter-spacing: 0%;
-    vertical-align: baseline;
-    position: absolute;
-    left: 5.8vw;
-    color: #344054;
-  }
-  .email-input-container {
-    font-size: 14px;
-    font-family: "Inter", sans-serif;
-    font-weight: 500;
-    line-height: 20px;
-    letter-spacing: 0%;
-    vertical-align: baseline;
-    position: absolute;
-    top: 3vh;
-    left: 5.8vw;
-  }
-  .email-input-style {
-    font-size: calc(0.4vw + 1vh);
-    border: 1px solid #d0d5dd;
-    border-radius: 5px;
-    padding: 10px;
-    width: 20vw;
-    height: 2vh;
-  }
-  .email-validation {
-    font-size: calc(0.4vw + 0.5vh);
-    font-family: "Inter", sans-serif;
-    font-weight: 400;
-    line-height: 20px;
-    letter-spacing: 0%;
-    vertical-align: baseline;
-    position: absolute;
-    left: 5.8vw;
-    color: red;
-    top: 7.5vh;
-    padding-top: 2.2px;
-  }
-  .password-container {
-    width: 60vh;
-    height: 10vh;
-    position: absolute;
-    top: 29vh;
-  }
-  .password-title {
-    font-size: calc(0.4vw + 1vh);
-    font-family: "Inter", sans-serif;
-    font-weight: 500;
-    line-height: 20px;
-    letter-spacing: 0%;
-    vertical-align: baseline;
-    position: absolute;
-    left: 5.8vw;
-    color: #344054;
-  }
-  .password-input-container {
-    font-size: 14px;
-    font-family: "Inter", sans-serif;
-    font-weight: 500;
-    line-height: 20px;
-    letter-spacing: 0%;
-    vertical-align: baseline;
-    position: absolute;
-    top: 3vh;
-    left: 5.8vw;
-  }
-  .password-input-style {
-    font-size: calc(0.4vw + 1vh);
-    border: 1px solid #d0d5dd;
-    border-radius: 5px;
-    padding: 10px;
-    width: 20vw;
-    letter-spacing: 2px;
-  }
-  .password-validation {
-    font-size: calc(0.4vw + 0.5vh);
-    font-family: "Inter", sans-serif;
-    font-weight: 400;
-    line-height: 2vh;
-    letter-spacing: 0%;
-    vertical-align: baseline;
-    position: absolute;
-    left: 5.8vw;
-    top: 7.5vh;
-    width: 32vh;
-    padding-top: 7px;
-  }
-  .verify-password-container {
-    width: 60vh;
-    height: 10vh;
-    position: absolute;
-    top: 42.5vh;
-  }
-  .verify-password-title {
-    font-size: calc(0.4vw + 1vh);
-    font-family: "Inter", sans-serif;
-    font-weight: 500;
-    line-height: 20px;
-    letter-spacing: 0%;
-    vertical-align: baseline;
-    position: absolute;
-    left: 5.8vw;
-    color: #344054;
-  }
-  .verify-password-input-container {
-    font-size: 14px;
-    font-family: "Inter", sans-serif;
-    font-weight: 500;
-    line-height: 20px;
-    letter-spacing: 0%;
-    vertical-align: baseline;
-    position: absolute;
-    top: 3vh;
-    left: 5.8vw;
-  }
-  .verify-password-input-style {
-    font-size: calc(0.4vw + 1vh);
-    border: 1px solid #d0d5dd;
-    border-radius: 5px;
-    padding: 10px;
-    width: 20vw;
-    letter-spacing: 2px;
-  }
-  .verify-password-validation {
-    font-size: calc(0.4vw + 0.5vh);
-    font-family: "Inter", sans-serif;
-    font-weight: 400;
-    line-height: 20px;
-    letter-spacing: 0%;
-    vertical-align: baseline;
-    position: absolute;
-    left: 5.8vw;
-    color: red;
-    top: 7.5vh;
-    padding-top: 2.2px;
-  }
-  .primary-button-container {
-    width: 60vh;
-    height: 5vh;
-    position: absolute;
-    top: 54vh;
-  }
-  .primary-button {
-    background-color: #1570ef;
-    color: #fff;
-    border: none;
-    border-radius: 8px;
-    padding: 10px 20px;
-    cursor: pointer;
-    transition: background-color 0.3s ease-in-out;
-    width: 21.5vw;
-    position: absolute;
-    left: 5.8vw;
-    font-weight: 575;
-    font-size: calc(0.5vw + 0.8vh);
-    height: 5vh;
-    text-align: center;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .secondary-button-container {
-    width: 60vh;
-    height: 10vh;
-    position: absolute;
-    top: 60.5vh;
-  }
-  .secondary-button {
-    height: 5vh;
-    font-size: calc(0.5vw + 0.8vh);
-    border: 1px solid #d0d5dd;
-    background-color: white;
-    color: #344054;
-    border-radius: 8px;
-    padding: 10px 20px;
-    cursor: pointer;
-    transition: background-color 0.3s ease-in-out;
-    width: 21.5vw;
-    position: absolute;
-    left: 5.8vw;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .secondary-button-label {
-    position: relative;
-    right: 0.8vw;
-  }
-  .log-in-container {
-    display: flex;
-    justify-content: space-between;
-    width: 60vh;
-    height: 10vh;
-    position: absolute;
-    top: 66vh;
-  }
-  .log-in-label {
-    color: #667085;
-    width: 50%;
-    padding: 10px;
-    font-size: calc(0.52vw + 0.5vh);
-    font-family: "Inter", sans-serif;
-    font-weight: 400;
-    line-height: 20px;
-    letter-spacing: 0%;
-    position: absolute;
-    left: 10.5vw;
-  }
-  .log-in-link {
-    color: #175cd3;
-    width: 50%;
-    padding: 10px;
-    font-size: calc(0.52vw + 0.6vh);
-    font-family: "Inter", sans-serif;
-    font-weight: 600;
-    line-height: 20px;
-    letter-spacing: 0%;
-    position: absolute;
-    left: 20vw;
-    cursor: pointer;
-  }
-</style>
+              <InputFieldWithValidation
+                     label="Email"
+                     placeholder="name@email.com"
+                     bind:value={email}
+                     validationMessage = {emailValidationString}
+                     validationActive = {emailValidationActive}
+                    
+                  />
+
+                  <InputFieldWithValidation
+                     label="Password"
+                     isPasswordType={true}
+                     placeholder="••••••••"
+                     bind:value={password}
+                     validationMessage = {passwordValidationString}
+                     validationActive = {passwordValidationActive}
+                     showValidation = {true}
+                  />
+
+                  <InputFieldWithValidation
+                     label="Verify Password"
+                     isPasswordType={true}
+                     placeholder="••••••••"
+                     bind:value={verifyPassword}
+                     validationMessage = {verifyPasswordValidationString}
+                     validationActive = {verifyPasswordValidationActive}
+                     
+                  />
+
+                  <div class="w-full h-[20px] mb-9 mt-8">
+                    <Button
+                       fullWidth={true}
+                       type="primary"
+                       text="Sign Up"
+                       handleClick={handleSignUp}
+                    
+                    />
+                 </div>
+
+                 <div>
+                    <form
+                       class="w-full h-[20px] mb-10"
+                       method="post"
+                       action="?/OAuth2"
+                    >
+                       <Button
+                          fullWidth={true}
+                          type="tertiary"
+                          text="Sign Up with Google"
+                          iconSvg={googleIcon}
+                       />
+                    </form>
+                 </div>
+
+                 <div class="w-full flex justify-center">
+                    <LoginPrompt
+                       text="Already have an account?"
+                       linkText="Log In"
+                       isPrimary = {false}
+                       handleClick={handleLogInClick}
+                      
+                    />
+                 </div>
+           </div>
+        </div>
+     </div>
+  </div>
+</div>
