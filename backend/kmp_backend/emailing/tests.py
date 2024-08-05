1ad2dfb22aafae86bed98eff6e762509c299155e
Ayesha Tariq <atar0014@student.monash.edu>
2024-08-05 20:21:35 +1000

    merge both test cases for contact and send_email together

--- backend/kmp_backend/emailing/tests.py
+++ backend/kmp_backend/emailing/tests.py
@@ -59,6 +59,38 @@ class TestEmailSent(TestCase):
         response = send_email(request)
         #determine if the status code is as expected
         self.assertEqual(response.status_code, 200)
+
+
+    @patch('emailing.views.ssl.create_default_context')
+    @patch('emailing.views.smtplib.SMTP_SSL')
+    def test_send_email_case_3(self, mock_create_default_context, mock_smtp_ssl):
+
+        mock_context = MagicMock()
+        mock_create_default_context.return_value = mock_context
+        mock_server = MagicMock()
+        mock_smtp_ssl.return_value = mock_server
+
+        #mock request created
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['message'] = "Testing message"
+        request.POST['subject'] = "Testing Subject"
+        request.POST['contacts'] = "at@test.com"
+
+        # calling the send_email function to generate response
+        response = send_email(request)
+        response_content = json.loads(response.content.decode('utf-8'))
+
+        # determining whether the default_context is called at least once
+        mock_create_default_context.assert_called_once()
+
+        # # Assert that the SMTP_SSL connection was established and used
+        # mock_smtp_ssl.assert_called_once_with("smtp.gmail.com", 465, context=mock_context)
+        # mock_server.login.assert_called_once_with("keepmeposted.monash@gmail.com", "aqsokzlapmzxvuev")
+        # mock_server.sendmail.assert_called_once()
+        # mock_server.quit.assert_called_once()
+
+
     
     def test_exception_raised_when_contacts_empty(self):
         request = HttpRequest()
