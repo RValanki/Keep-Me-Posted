792bd6482341d23ad40e4120e4b688f98f9dec55
ZWAN0318 <zwan0318@student.monash.edu>
2024-08-05 15:19:54 +1000

    Added all required assertion and post response for transcribe test

--- backend/kmp_backend/assemblyaimodule/tests.py
+++ backend/kmp_backend/assemblyaimodule/tests.py
@@ -2,13 +2,16 @@ from django.test import TestCase
 from gtts import gTTS
 from io import BytesIO
 from . import assemblyAI_module as tai
-from . import views
+from assemblyaimodule import views
 import unittest
 from unittest.mock import patch, Mock
+from rest_framework.response import Response
+from rest_framework import status
 
 class TranscribeTestCase(TestCase):
+    # Setup API client to post requests for mocking
     def setUp(self):
-        self.factory = APIRequestFactory()
+        self.client = APIClient()
 
     @staticmethod
     # Helper function to generate audio files in memory
@@ -31,6 +34,7 @@ class TranscribeTestCase(TestCase):
         # Mock serializer object as valid
         mock_serializer = MockSerializer.return_value
         mock_serializer.is_valid.return_value = True
+        mock_serializer.validated_data = {'audio_file': MagicMock()}
 
         # Mock uploaded file
         mock_storage = MockStorage.return_value
@@ -40,6 +44,20 @@ class TranscribeTestCase(TestCase):
         # Mock transcription tesult
         mock_transcription_result = "I am testing, testing."
         MockTranscribe.return_value = mock_transcription_result
+
+        # Mock a POST request
+        response = self.client.post('/transcribe/', {'audio_file': MagicMock()})
+
+        # Assert the response
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {'transcription': mock_transcription_result})
+
+        # Assert frequency of all mock calls
+        MockAudioFileSerializer.assert_called_once_with(data={'audio_file': MagicMock()})
+        mock_serializer.is_valid.assert_called_once()
+        mock_fs.save.assert_called_once_with(mock_serializer.validated_data['audio_file'].name, mock_serializer.validated_data['audio_file'])
+        MockTranscribe.assert_called_once_with(mock_saved_file_url)
+        mock_fs.delete.assert_called_once_with(mock_saved_file_url)
 
         
     # Test 1: Test return type of transcribe()
