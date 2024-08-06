6fb38ddfe7955f57f8b3d5af0da6a456bcf74e65
ZWAN0318 <zwan0318@student.monash.edu>
2024-08-06 18:53:12 +1000

    Fixed django model issue with AudioFile

--- backend/kmp_backend/assemblyaimodule/urls.py
+++ backend/kmp_backend/assemblyaimodule/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path('api/transcribe', views.transcribe),
-    path('api/transcribe/speakers', views.transcribe_with_speakers),
+    path('api/transcribe', views.transcribe, name='transcribe'),
+    path('api/transcribe/speakers', views.transcribe_with_speakers, name='transcribe_with_speakers'),
 ]
 
