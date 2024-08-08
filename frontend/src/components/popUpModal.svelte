1d873d60ddf6b93aa3fe63ba6d8277b7336bbe3f
bowendong1234 <126026189+bowendong1234@users.noreply.github.com>
2024-08-09 09:03:27 +1000

    Feature/86cw4p95c error popup (#78)
    * Initial commit with new component
    
    * Styled error pop-up
    
    * Implemented dismissal of error
    
    * Added documentation
    
    * Added documentation of example usage
    
    * Refactor pop up modal to accomdate error
    
    * Refactor error pop up to use pop up modal
    
    * Minor bug fixes, added documentation, removed error pop up

--- frontend/src/components/popUpModal.svelte
+++ frontend/src/components/popUpModal.svelte
@@ -3,7 +3,8 @@
     Contains pop-up modal
 
     Author: Brenda Dang
-    Last modified: 8/08/2024
+    Modified by: Bowen Dong
+    Last modified: 9/08/2024
 
 -->
 
@@ -17,28 +18,79 @@
     export let mainText = 'This is a pop-up modal.';  // Main text of popup modal
     export let firstButtonText = 'Button';  // Text for primary button (dark blue, or red)
     export let secondButtonText = 'Button';  // Text for secondary button (light blue)
+    export let iconPath = ''; // path for image icon
+    export let width = '64'; // default width of pop up
+
 </script>
+
 
 <div class="justify-center items-center font-sans h-full transition ease-in-out duration-300">
     <div class="fixed inset-0 flex items-center justify-center w-full h-full bg-black backdrop-blur md:bg-opacity-70">
-        <div class="bg-white rounded-lg p-4 w-64">
+        <div class="bg-white rounded-lg p-4 w-{width}">
+            {#if iconPath}
+                <div class="flex items-center justify-center pb-2">
+                    <img src={iconPath} alt="pop up icon"/>
+                </div>  
+            {/if}
             <div class="flex items-center justify-center text-gray-900 text-lg font-bold">
                 {header}
             </div>
-            <div class="flex items-center justify-center text-gray-500 text-sm pb-3 pt-2">
+            <div class="flex items-center justify-center text-gray-500 text-sm pb-5 pt-2">
                 {mainText}
             </div>
-            <div class="flex items-center justify-between flex-row">
-                {#if type === 'secondary'}
+            {#if type === 'secondary'}
+                <div class="flex items-center justify-between flex-row">
                     <Button type="secondary" text={secondButtonText} handleClick={secondHandleClick} />
-                {/if}
-                <Button class="
-                {type === 'error' ? '' : 'primary'}"
-                fullWidth= {type === 'primary' ? true : false}
-                text={firstButtonText} 
-                handleClick={firstHandleClick}
-                />
-            </div>
+                    <Button type=
+                    {type === 'error' ? 'error' : 'primary'}
+                    fullWidth= {type === 'primary' || type === 'error' ? true : false}
+                    text={firstButtonText} 
+                    handleClick={firstHandleClick}
+                    />
+                </div>
+            {:else}
+                <div class="w-full">
+                    <Button type=
+                    {type === 'error' ? 'error' : 'primary'}
+                    fullWidth= {type === 'primary' || type === 'error' ? true : false}
+                    text={firstButtonText} 
+                    handleClick={firstHandleClick}
+                    />
+                </div>
+            {/if}
         </div>
     </div>
 </div>
+
+<!--
+    EXAMPLE USAGE FOR ERROR POP UP
+<script>
+    import TopBar from "../../components/topbar.svelte"
+    import PopUpModal from "../../components/popUpModal.svelte";
+
+    let ifError = false
+
+    function dismissError() {
+        ifError = false;
+    }
+
+</script>
+
+
+<body>
+    <TopBar />
+    <button on:click={() => ifError = !ifError}>click to cause error</button>
+    
+    {#if ifError}
+        <PopUpModal 
+        header="Invalid audio format!"
+        mainText="Your meeting audio must be in MP3 or WAV format."
+        type='error'
+        iconPath='../src/assets/error-icon.svg'
+        firstButtonText="Re-upload"
+        firstHandleClick={dismissError}
+        width='96'/>
+    {/if}
+
+</body>
+-->
