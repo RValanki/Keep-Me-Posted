71a9adedda74fb5aeeb50a41091766bf457e3b0b
Parul Garg <pgar0011@student.monash.edu>
2024-08-08 13:43:42 +1000

    completed the styling of the page

--- frontend/src/routes/send_summary_transcript/+page.svelte
+++ frontend/src/routes/send_summary_transcript/+page.svelte
@@ -22,90 +22,39 @@
         goto("/upload-audio")
     } 
   
-    </script>
+</script>
     
-      <Topbar> </Topbar>
+<Topbar> </Topbar>
+
+<div class="flex flex-col items-center pt-3 pb-10">
   
-      <div class="flex flex-col items-center pt-3 pb-10">
-        
+
+  <div class="flex flex-col items-center pt-10">
+    <div class="flex justify-center">
+      <img class="flex flex-row justify-center items-center p-0 bg-indigo-200 flex-none order-none flex-grow-0 w-24 h-24" src={clock} alt="Clock" />
+    </div>
+    <h1 class="pt-3">Your Summary and Transcript Will Be Sent..</h1>
+    <p class="pt-5">After the summary has been completely generated, it will be sent automatically with the transcript.</p>
+  </div>
   
-        <div class="title-container">
-          <div class="flex justify-center">
-            <img class="flex flex-row justify-center items-center p-0 bg-indigo-200 flex-none order-none flex-grow-0 w-24 h-24" src={clock} alt="Clock" />
-          </div>
-          <h1 class="pt-3">Your Summary and Transcript Will Be Sent..</h1>
-          <p class="subheading">After the summary has been completely generated, it will be sent automatically with the transcript.</p>
-        </div>
-        
-        <div class="all-recipients-container">
-          <div class="recipients-txt">Recipients:</div>
-    
-          <div class="recipients-container">
-            
-            {#each $ContactsStore as email}
-            <div class="email-token">
-              <div class="token">{email}</div>
-            </div>
-          {/each}
-          </div>
+  <div class="flex pt-10 gap-3.125">
+    <div class="font-bold">Recipients:</div>
+
+    <div class="flex flex-wrap gap-0.3125">
+      
+      {#each $ContactsStore as email}
+      <div class="flex bg-gray-100 p-1 rounded justify-center items-center gap-3.125">
+        <div class="token">{email}</div>
       </div>
-  
-      </div>
-  
-      <div class="button-container">
-        <div class="button-holder">
-          <Button handleClick={nextPage} text="Send Another Summary"></Button>
-        </div>
-      </div>
+    {/each}
+    </div>
+  </div>
+</div>
 
-  <style>
 
-  
-    .button-container {
-      position: absolute;
-      right: 45vw;
-      bottom: 10px;
-    }
-  
-    .title-container {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      padding-top: 3%
-    }
+<div class="absolute bottom-10 left-1/2 transform -translate-x-1/2">
+  <div class="button-holder">
+    <Button handleClick={nextPage} text="Send Another Summary"></Button>
+  </div>
+</div>
 
-    .all-recipients-container {
-      display: flex;
-      gap: 50px;
-      padding-top: 3%;
-
-    }
-  
-    .recipients-container {
-      display: flex;
-      flex-wrap: wrap;
-      max-width: 40%;
-      gap: 5px;
-    }
-  
-    .email-token {
-      display: flex;
-      background-color: #F8F9FC;
-      padding: 4px;
-      border-radius: 5px;
-      justify-content: center;
-      align-items: center;
-      gap: 3px;
-    }
-    .subtitle {
-      padding-bottom: 3%;
-    }
-
-    .subheading {
-      padding-top: 3%;
-    }
-    .recipients-txt{
-      font-weight: 700;
-    }
-  
-  </style>
