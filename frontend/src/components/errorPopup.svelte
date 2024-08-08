484960fa5e66a0f8e47e2ca9b958b80bbb561024
bowendong1234 <bdon0009@student.monash.edu>
2024-08-09 08:45:35 +1000

    Refactor error pop up to use pop up modal

--- frontend/src/components/errorPopup.svelte
+++ frontend/src/components/errorPopup.svelte
@@ -7,7 +7,7 @@
 -->
 
 <script>
-    import Button from "./button.svelte";
+    import PopUpModal from "./popUpModal.svelte";
     export let errorSubHeadingText; // this is the main text of the card (e.g. 'Your meeting audio should be less than 120 minutes')
     export let errorHeadingText; // this is the heading (e.g. 'Meeting duration exceeded!')
     export let errorButtonText; // this is the button text (e.g. 'Re-upload')
@@ -20,28 +20,16 @@
 </script>
 
 {#if isVisible}
-<div class="justify-center items-center font-sans h-full transition ease-in-out duration-300">
-    <div class="fixed inset-0 flex items-center justify-center w-full h-full bg-black backdrop-blur md:bg-opacity-70">
-        <div class="bg-white rounded-lg p-4 w-96 flex flex-col justify-center items-center">
-            <img src={'../src/assets/error-icon.svg'} alt="error icon"/>
-            <div class="flex items-center justify-center text-gray-900 text-lg font-bold pt-2">
-                {errorHeadingText}
-            </div>
-            <div class="text-gray-500 text-sm pb-3 pt-2">
-                {errorSubHeadingText}
-            </div>
-            <div class="pt-4 w-full">
-                <Button
-                type='error'
-                fullWidth = {true}
-                text={errorButtonText} 
-                handleClick={dismissError}
-                />
-            </div>
-        </div>
-    </div>
-</div>
+    <PopUpModal
+    header={errorHeadingText}
+    mainText={errorSubHeadingText}
+    type='error'
+    iconPath='../src/assets/error-icon.svg'
+    firstButtonText={errorButtonText}
+    firstHandleClick={dismissError}
+    width='96'/>
 {/if}
+
 
 <!--
     EXAMPLE USAGE ON A PAGE
