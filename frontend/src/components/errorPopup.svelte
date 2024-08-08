698a5c2b0c055a925187ed2bd66828339d962f7f
bowendong1234 <bdon0009@student.monash.edu>
2024-08-08 11:04:25 +1000

    Initial commit with new component

---
+++ frontend/src/components/errorPopup.svelte
@@ -0,0 +1,23 @@
+<!-- Error Popup Component
+
+    Contains an error pop-up that can be modified
+
+    Author: Bowen Dong
+    Last Modified: 
+-->
+
+<script>
+    export let errorSubHeadingText; // this is the main text of the card (e.g. 'Your meeting audio should be less than 120 minutes')
+    export let errorHeadingText; // this is the heading (e.g. 'Meeting duration exceeded!')
+    export let errorButtonText; // this is the button text (e.g. 'Re-Upload')
+    export let handleErrorClick = () => {}; // pass a method here that does what you want to happen when you click the button
+    
+    function handleClick() {
+        handleCardClick();
+    }
+
+</script>
+  
+<div>
+    
+</div>
