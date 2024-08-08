ebc740ea071c103e3e6cdebc93ff9e6b73a98303
bowendong1234 <bdon0009@student.monash.edu>
2024-08-08 12:27:40 +1000

    Added documentation of example usage

--- frontend/src/components/errorPopup.svelte
+++ frontend/src/components/errorPopup.svelte
@@ -10,7 +10,7 @@
     import Button from "./button.svelte";
     export let errorSubHeadingText; // this is the main text of the card (e.g. 'Your meeting audio should be less than 120 minutes')
     export let errorHeadingText; // this is the heading (e.g. 'Meeting duration exceeded!')
-    export let errorButtonText; // this is the button text (e.g. 'Re-Upload')
+    export let errorButtonText; // this is the button text (e.g. 'Re-upload')
     export let isVisible = false; // when error occurs, change to true
     
     function dismissError() {
@@ -42,3 +42,19 @@
     </div>
 </div>
 {/if}
+
+<!--
+    EXAMPLE USAGE ON A PAGE
+-->
+
+<!-- <script>
+    import ErrorPopup from "../../components/errorPopup.svelte";
+
+    let ifError = false
+</script>
+
+
+<body>
+    <button on:click={() => ifError = !ifError}>click to cause error</button>
+   <ErrorPopup errorHeadingText="Invalid audio format!" errorSubHeadingText="Your meeting audio must be in MP3 or WAV format." errorButtonText="Re-upload" isVisible={ifError}/>
+</body> -->
