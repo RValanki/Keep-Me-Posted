9581ce7a2909abcf3795a7639bce944fc1b5dfb6
Jeremy Wang <zwan0318@student.monash.edu>
2024-08-11 17:35:29 +1000

    Fully mock tested transcribe view

--- backend/kmp_backend/assemblyaimodule/tests.py
+++ backend/kmp_backend/assemblyaimodule/tests.py
@@ -1,55 +1,74 @@
-from django.test import TestCase, Client
 from django.urls import reverse
-from gtts import gTTS
-from io import BytesIO
-from . import assemblyAI_module as tai
-from assemblyaimodule import views, models
-from unittest.mock import patch, MagicMock
-from rest_framework.response import Response
+from assemblyaimodule import views
+from unittest.mock import patch
+from rest_framework.test import APITestCase
 from rest_framework import status
 
-class TranscribeTestCase(TestCase):
-    # Setup API client to post requests for mocking
-    def setUp(self):
-        self.client = Client()
-    
-    # Test 1: Test transcribe() 
-    @patch('assemblyaimodule.views.TS.transcribe')
+class TranscribeTestCase(APITestCase):
+
+    # Test 1: Test transcribe() invalid
     @patch('assemblyaimodule.views.AudioFileSerializer')
+    def test_transcribe_invalid(self, MockSerializer):
+
+        # Mock serializer object as invalid with error
+        mock_serializer = MockSerializer.return_value
+        mock_serializer.is_valid.return_value = False
+        test_error = "This is a testing error"
+        mock_serializer.errors = test_error
+
+        # Mock a POST request
+        response = self.client.post(reverse('transcribe'))
+
+        # Assert the response
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, test_error)
+
+        # Assert function calls
+        MockSerializer.assert_called_once()
+    
+    # Test 2: Test transcribe() valid
+    @patch('assemblyaimodule.views.TS.transcribe')
     @patch('assemblyaimodule.views.FileSystemStorage')
-    def test_transcribe(self, MockTranscribe, MockSerializer, MockStorage):
+    @patch('assemblyaimodule.views.AudioFileSerializer')
+    def test_transcribe_valid(self, MockSerializer, MockStorage, MockTranscribe):
+
         # Mock serializer object as valid
         mock_serializer = MockSerializer.return_value
         mock_serializer.is_valid.return_value = True
-        mock_serializer.validated_data['audio_file'] = "test audio"
-        mock_serializer.errors = "Error"
 
         # Mock uploaded file
         mock_storage = MockStorage.return_value
-        mock_url = views.UPLOAD_DIRECTORY_PATH + "test"
-        mock_storage.save.return_value = mock_url
+        mock_storage.save.return_value = "saved_file"
 
         # Mock transcription tesult
-        mock_transcribe = MockTranscribe.return_value
-        mock_transcription_result = "I am testing, testing."
-        mock_transcribe.return_value = mock_transcription_result
-        mock_storage.delete = MagicMock(name='delete')
+        test_text = "I am testing, testing."
+        transcription_result = {'transcription': test_text}
+        MockTranscribe.return_value = test_text
 
         # Mock a POST request
-        mock_file = MagicMock()
-        mock_file.data = "test data"
-        response = self.client.post(reverse('transcribe'), {'audio_file': mock_file})
-
-        print(f"Response status code: {response.status_code}")
-        print(f"Response data: {response.data}")
+        response = self.client.post(reverse('transcribe'))
 
         # Assert the response
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, {'transcription': mock_transcription_result})
+        self.assertEqual(response.data, transcription_result)
 
-        # Assert frequency of all mock calls
-        mock_serializer.assert_called_once_with(data={'audio_file': MagicMock()})
+        # Assert function calls  
+        MockSerializer.assert_called_once()
         mock_serializer.is_valid.assert_called_once()
-        mock_storage.save.assert_called_once_with(mock_serializer.validated_data['audio_file'].name, mock_serializer.validated_data['audio_file'])
-        MockTranscribe.assert_called_once_with(mock_url)
-        mock_storage.delete.assert_called_once_with(mock_url)
+        MockStorage.assert_called_once()
+        mock_storage.save.assert_called_once()
+        mock_storage.save.assert_called_with(mock_serializer.validated_data['audio_file'].name, mock_serializer.validated_data['audio_file'])
+        mock_storage.delete.assert_called_once()
+        mock_storage.delete.assert_called_with(mock_storage.save.return_value)
+        MockTranscribe.assert_called_once()
+        MockTranscribe.assert_called_with(views.UPLOAD_DIRECTORY_PATH + mock_storage.save.return_value)
+
+
+
+
+
+        
+
+    
+
+        
