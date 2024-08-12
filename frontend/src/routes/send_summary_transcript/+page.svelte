8c013697b5c74be60ec9467e80b92f1ecf344383
Parul Garg <pgar0011@student.monash.edu>
2024-08-12 10:27:50 +1000

    added comments and made changes according to the review comment on the pull request

--- frontend/src/routes/send_summary_transcript/+page.svelte
+++ frontend/src/routes/send_summary_transcript/+page.svelte
@@ -6,8 +6,6 @@
     Last Modified: 10/08/2024
 -->
 
-
-
 <script>
     import Button from "../../components/button.svelte";
     import Topbar from "../../components/topbar.svelte";
@@ -15,7 +13,6 @@
     import { goto } from "$app/navigation";
     import Recipients from "../../components/sendRecipientsList.svelte";
 
-    
     //the next page in the sequence
     let nextPage = () => {
         console.log("clicked");
@@ -26,28 +23,30 @@
     
 <Topbar> </Topbar>
 
+<!-- The div for the content of the page excluding the button -->
 <div class="flex flex-col items-center pt-3 pb-10">
   
-
+  <!-- The div for the headings and the clock icon of the page -->
   <div class="flex flex-col items-center pt-10">
+    <!-- styling of the clock icon -->
     <div class="flex justify-center">
       <img class="flex flex-row justify-center items-center p-0 bg-indigo-200 flex-none order-none flex-grow-0 w-24 h-24" src={clock} alt="Clock" />
     </div>
+    <!-- the heading and subheading of the page -->
     <h1 class="pt-3">Your Summary and Transcript Will Be Sent..</h1>
     <p class="pt-5">After the summary has been completely generated, it will be sent automatically with the transcript.</p>
   </div>
   
+  <!-- The div for the recipients section of the page -->
   <div class="flex pt-10 gap-3.125">
     <div class="font-bold pr-8">Recipients:</div>
-
     <div class="flex flex-wrap">
       <Recipients></Recipients>
     </div>
-
   </div>
 </div>
 
-
+<!-- The button for sending another summary -->
 <div class="absolute bottom-10 left-1/2 transform -translate-x-1/2">
   <div class="button-holder">
     <Button handleClick={nextPage} text="Send Another Summary"></Button>
