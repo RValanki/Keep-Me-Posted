29b4c6b0c86725fa3c3e07c0fd6675e61da8bfef
Harrison Lane <hlan0016@student.monash.edu>
2024-08-17 01:40:03 +1000

    adding a reset function that resets all the stores when the user clicks on upload another audio

---
+++ frontend/src/stores/reset-store.js
@@ -0,0 +1,24 @@
+import { apiStatusStore } from "./api-status-store";
+import { contactsStore } from "./contacts-store";
+import { emailStatusStore } from "./email-status-store";
+import { summaryStore} from "./summary-store";
+import { transcriptStore, sendWithTranscriptStore } from "./transcript-store";
+import { isOpen, isCancelled } from "./user-email-popup-store";
+
+export function resetStores() { 
+    
+    apiStatusStore.set("");
+    contactsStore.set([]);
+    emailStatusStore.set("")
+    summaryStore.set({
+        summary: "",
+        subject: ""
+    })
+    transcriptStore.set({
+        transcript: ""
+    })
+    sendWithTranscriptStore.set(false)
+    isOpen.set(false)
+    isCancelled.set(false)
+
+}
