64d10ebfa80b7539cd083851ba12adff0eaa3f32
Ahmed Almasry <aalm0027@student.monash.edu>
2024-08-18 20:32:40 +1000

    temporarily prevent cypress from failing due to uncaught exceptions

--- frontend/cypress/support/e2e.js
+++ frontend/cypress/support/e2e.js
@@ -18,3 +18,9 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+// Temporarily prevent uncaught exceptions from failing tests
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+  });
+  
