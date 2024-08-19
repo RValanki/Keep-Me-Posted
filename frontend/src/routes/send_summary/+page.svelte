492ccf9eb1be5f25adca88c47248b5091c6332ed
benjicherian <bche0062@student.monash.edu>
2024-08-19 15:19:09 +1000

    removed attachment of transcript to body of email

--- frontend/src/routes/send_summary/+page.svelte
+++ frontend/src/routes/send_summary/+page.svelte
@@ -34,10 +34,10 @@
       if ($apiStatusStore == "Complete") {
         let summary = $summaryStore.summary 
 
-        if ($sendWithTranscriptStore == true) {
-          console.log("adding transcript")
-          summary += '\n\nTranscript: ' + $transcriptStore.transcript
-        }
+        // if ($sendWithTranscriptStore == true) {
+        //   console.log("adding transcript")
+        //   summary += '\n\nTranscript: ' + $transcriptStore.transcript
+        // }
 
         console.log(summary)
         
