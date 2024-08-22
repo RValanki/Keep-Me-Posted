ceb7dc15fa5053d3391ffc5e92b4a15ac5f770fd
Alex Ung <aung0008@student.monash.edu>
2024-08-22 18:25:02 +1000

    removing redundant code and documentation

--- frontend/cypress/e2e/Test Google Auth/auth.cy.js
+++ frontend/cypress/e2e/Test Google Auth/auth.cy.js
@@ -1,19 +1,4 @@
-// cypress/integration/oauth.spec.js
-
-describe('Home Page', () => {
-    // Define the base URL if not set in Cypress config
-    const baseUrl = 'http://localhost:5173';
-  
-    // Run before each test
-    beforeEach(() => {
-        cy.visit(baseUrl + "/login"); // Visit the login page
-    });
-
-    it('should display the Google sign-in button', () => {
-        // Check if the Google sign-in button is visible
-        cy.get('#google-button').should('be.visible').and("exist");
-    });
-});
+//Testing file for google authentication
 
 describe('OAuth2 Login Flow', () => {
     // Define the base URL if not set in Cypress config
@@ -53,6 +38,7 @@ describe('OAuth2 Login Flow', () => {
         // Simulate the callback with the code
         cy.visit(baseUrl + '/oauth?code=fake_auth_code');
 
+        //This can be uncommented when the user information is collected.
         // // Mock the user info request to Google API
         // cy.intercept('GET', 'https://www.googleapis.com/oauth2/v3/userinfo?access_token=fake_access_token', {
         //     statusCode: 200,
@@ -71,31 +57,4 @@ describe('OAuth2 Login Flow', () => {
         cy.url().should('include', '/upload_audio');
 
     });
-
-    // it('should display an error message on OAuth2 failure', () => {
-    //     // Mock an error during OAuth2 token exchange
-    //     cy.intercept('GET', '**/oauth**', {
-    //         statusCode: 400,
-    //         body: { error: 'invalid_grant' },
-    //     }).as('oauthErrorRequest');
-
-    //     // Click the Google sign-in button
-    //     cy.get('#google-button').click();
-
-    //     // Simulate the callback with an invalid code using cy.request()
-    //     cy.request({
-    //         url: '/oauth?code=invalid_code',
-    //         failOnStatusCode: false // This prevents Cypress from failing the test on non-2xx status codes
-    //     }).then((response) => {
-    //         // Ensure the OAuth2 token exchange request failed
-    //         expect(response.status).to.eq(400);
-    //         // Check for an error message in the response
-    //         cy.get('.error-message').should('be.visible').and('contain', 'Authentication failed');
-    //     });
-    //     // Ensure the OAuth2 token exchange request failed
-    //     cy.wait('@oauthErrorRequest').its('response.statusCode').should('eq', 400);
-
-    //     // Check for an error message on the page
-    //     cy.get('.error-message').should('be.visible').and('contain', 'Authentication failed');
-    // });
 });
