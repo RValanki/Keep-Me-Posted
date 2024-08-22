c87ab2ac0e97b51206eccc156fcd342f053003ca
Ayesha Tariq <atar0014@student.monash.edu>
2024-08-22 23:47:51 +1000

    updated cypress.config back to what it was because it did not pass cypress check on github

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from "cypress";
-import fileUpload from "cypress-file-upload";
-
 
 export default defineConfig({
   projectId: '2ciyzj',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      on('file:preprocessor', fileUpload()); // Registering the file upload plugin
     },
     baseUrl: 'http://localhost:5173',
   },
