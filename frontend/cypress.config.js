432c72e6769f4767295fd81e2c43c734d3e7a2f5
Ayesha Tariq <atar0014@student.monash.edu>
2024-08-22 23:45:03 +1000

    updated cypress.config and package.json file to include file upload plugin for cypress

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from "cypress";
+import fileUpload from "cypress-file-upload";
+
 
 export default defineConfig({
   projectId: '2ciyzj',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('file:preprocessor', fileUpload()); // Registering the file upload plugin
     },
     baseUrl: 'http://localhost:5173',
   },
