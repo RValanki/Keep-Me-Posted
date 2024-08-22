e4759878e2d77dae257894151101d99b05b34d1e
afia-farzana <afar0035@student.monash.edu>
2024-08-22 17:07:26 +1000

    Shift waiting period

--- frontend/cypress/e2e/Test Send Another Summary.cy.js
+++ frontend/cypress/e2e/Test Send Another Summary.cy.js
@@ -3,11 +3,11 @@ describe('Summary Sent Page', () => {
     // Visit the summary sent page before each test
     beforeEach(() => {
       cy.visit("/sent"); 
+      // Add a waiting period 
+      cy.wait(10000)
     });
     // Test the Send Summary Button
     it('should redirect to the send email page after clicking the "send another summary" button', () => {
-      // Add a waiting period 
-      cy.wait(10000)
       // Click the button labelled "Send Another Summary"
       cy.contains('button','Send Another Summary').click()
       // Verify that the URL contains "/email" after clicking the button
