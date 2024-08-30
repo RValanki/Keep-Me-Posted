f981e4a116d4615ee9cafca9db69bc6a5e95eeab
Rohit Valanki <rval00008@student.monash.edu>
2024-08-30 20:50:41 +1000

    Added header documentation for missing components

--- frontend/src/components/loginCardHeader.svelte
+++ frontend/src/components/loginCardHeader.svelte
@@ -1,12 +1,19 @@
+<!-- Login Card Header Component
+
+    Contains Header and Subheader component used for login/signup card
+
+    Author: Rohit Valanki
+    Last modified: 31/07/2024
+
+-->
 <script>
-    export let heading = 'Heading.';
-    export let subheading = 'Subheading.';
-  </script>
-  
-  <div class="text-[#101828] text-3xl font-inter font-[575] mb-2">
-    {heading}
-  </div>
-  <div class="text-[#667085] text-sm font-inter font-[350] mb-6">
-    {subheading}
-  </div>
-  
+  export let heading = "Heading.";
+  export let subheading = "Subheading.";
+</script>
+
+<div class="text-[#101828] text-3xl font-inter font-[575] mb-2">
+  {heading}
+</div>
+<div class="text-[#667085] text-sm font-inter font-[350] mb-6">
+  {subheading}
+</div>
