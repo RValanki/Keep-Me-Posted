42aa73e97417e52892ab38f7242806d090ee3169
Ahmed Almasry <aalm0027@student.monash.edu>
2024-09-10 23:39:04 +1000

    login with google test page

---
+++ frontend/src/routes/test_picker/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { onMount } from 'svelte'
+
+
+  const clientId = import.meta.env.SECRET_CLIENT_ID;
+
+  onMount(async () => {
+      const handleCredentialResponse = async (response) => {
+          console.log('Encoded JWT ID token: ' + response.credential);
+          };
+      google.accounts.id.initialize({
+          client_id: clientId,
+          callback: handleCredentialResponse,
+      });
+      google.accounts.id.renderButton(
+          document.getElementById('buttonDiv'),
+          { theme: 'outline', size: 'large' } // customization attributes
+      );
+      google.accounts.id.prompt();
+  });
+
+</script>
+
+<svelte:head>
+  <script src="https://accounts.google.com/gsi/client"></script>
+</svelte:head>
+
+<main>
+  <h1>Demo for <a href=https://stackoverflow.com/questions/72411045/google-auth-oauth-2-0-sveltekit-wierd-behavior>Google Auth OAuth 2.0 SvelteKit wierd behavior</a></h1>
+
+  <p><a href=https://github.com/Leftium/google-signin-kit>View on Github</a></p>
+
+  <div id=buttonDiv></div>
+</main>
