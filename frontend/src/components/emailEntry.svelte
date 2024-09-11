3934131f9a91568bbcad2506ead6f2eee16eb5c2
Parul Garg <pgar0011@student.monash.edu>
2024-09-11 20:27:47 +1000

    created the autofill suggestion box for emails; currently the list has dummy values

--- frontend/src/components/emailEntry.svelte
+++ frontend/src/components/emailEntry.svelte
@@ -26,20 +26,32 @@
 </script>
 
 <div class="flex justify-center gap-2 px-4">
-    <div class="relative">
-      <input
-        class="border border-slate-300 pl-9 p-2 rounded-xl w-full sm:w-96"
-        type="email"
-        placeholder="johndoe@email.com"
-        bind:value={emailString}
-      />
-      <button
-            class="absolute cursor-default h-5 w-5 left-3 top-3 py-2 flex items-center bg-white"
-            type="button">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.9" stroke="#667085" class="size-12">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
-          </svg>
-      </button>
+    <div class="relative border border-slate-300 pl-9 p-2 rounded-xl w-full sm:w-96 text-left">
+      <div class = "border-b">
+        <input
+          class="border-none pt-0 pl-0"
+          type="email"
+          placeholder="johndoe@email.com"
+          bind:value={emailString}
+        />
+        <button
+              class="absolute cursor-default h-5 w-5 left-3 top-3 pb-1 flex items-center bg-white"
+              type="button">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.9" stroke="#667085" class="size-12">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+            </svg>
+        </button>
+      </div>
+      <div class = "py-2 cursor-pointer">
+          <ul class="">
+            <li>
+              number 1
+            </li>
+            <li>
+                number 2
+            </li>
+          </ul>
+      </div>
     </div>
   <Button
     handleClick={addEmail}
