1fcf456da43a58b639eea83cb79c3c9d6c78ef30
Brenda Dang <bdan0005@student.monash.edu>
2024-09-15 01:23:36 +1000

    Added new store to fix auto-routing bug

--- frontend/src/components/uploadAudioBox.svelte
+++ frontend/src/components/uploadAudioBox.svelte
@@ -21,7 +21,8 @@
 	import LoadingBar from "./loadingBar.svelte";
 	import { transcribe_audio } from "../api-functions/transcribe_audio";
 	import { send_summary } from "../api-functions/send_summary";
-	import { apiStatusStore } from "../stores/api-status-store"
+	import { apiStatusStore } from "../stores/api-status-store";
+	import { navStatusStore } from "../stores/nav-status-store";
 	import { backendURL } from "../api-functions/base-URL"
 	import PopUpModal from "./popUpModal.svelte"; // Import the PopUpModal component
     import { resetStores } from "../stores/reset-store";
@@ -100,7 +101,10 @@
 				return Promise.reject("Upload cancelled")
 			}
 			apiStatusStore.set("Complete");
-			goto("/generate_summary")
+			if ($navStatusStore != "Skipped") {
+				goto("/generate_summary");
+			};
+			navStatusStore.set("");
 			console.log('Summary Received');
 		}).catch(error => {
 			if (error == "Upload cancelled") {
